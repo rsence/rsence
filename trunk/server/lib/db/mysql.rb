742387f322e24a965cce0d884c390576e0aa67e1
Juha-Jarmo Heinonen <o@rsence.org>
2008-02-26 14:59:28 +0000

    updated mysql connector
    git-svn-id: http://svn.rsence.org@37 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/server/lib/db/mysql.rb
+++ trunk/server/lib/db/mysql.rb
@@ -1,154 +1,190 @@
+###
+  # HIMLE RIA Server
+  # Copyright (C) 2008 HIMLE GROUP http://himle.sorsacode.com/
+  #  
+  #  This program is free software; you can redistribute it and/or modify it under the terms
+  #  of the GNU General Public License as published by the Free Software Foundation;
+  #  either version 2 of the License, or (at your option) any later version. 
+  #  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  #  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  #  See the GNU General Public License for more details. 
+  #  You should have received a copy of the GNU General Public License along with this program;
+  #  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+  ###
+
+#    This file is part of ROXOR, see http://roxor.sorsacode.com/
+
+#    ROXOR is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+
+#    ROXOR is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#    Copyright 2003, 2004, 2005, 2007 Juha-Jarmo Heinonen
 
 ### ROXOR MySQL Abstractor
 ##  by  Juha-Jarmo Heinonen (otus@olen.to)
-##
-## (c) 2003-2005 Juha-Jarmo Heinonen, All Rights Reserved
 ###
+
 
 require "dbi"
 
-### Database abstraction
-
-=begin
-
-db/MySQL Quick API
-
- class MySQL(
-                host[str],     <-= hostname/ip
-                user[str],     <-= db username
-                pass[str],     <-= db password
-                debe[str]      <-= database (optional)
-   ):
-   hash-array/int q(qu[str])  <-= Returns the results of the query string (or row id)
-   nil close_conn             <-= Close connections
-   str-array dbs              <-= Return names of available databases
-   nil db(nimi[str])          <-= Connect to / Create new database (new if not avail)
-   
-=end
-
-
-class MySQL
-  
-  ## Init with the database name
-  def initialize( setup )
-    
-    @host = setup[:host]  # Sets the host name as a instance variable
-    @user = setup[:user]  # Sets the database username as a instance variable
-    @pass = setup[:pass]  # Sets the database password as a instance variable
-    @debe = setup[:db]  # Sets the database name as a instance variable
-    
-    @conn = false # The connection is opened only when needed
-    
+class Status_TBL
+  attr_reader :name, :rows, :size_avg, :size, :last_id, :created, :modified, :comment
+  def initialize(row)
+    @name     = row['Name']
+    @rows     = row['Rows']
+    @size_avg = row['Avg_row_length']
+    @size     = row['Data_length']
+    @last_id  = row['Auto_increment']
+    ct = row['Create_time']
+    creat = Time.gm(ct.year,ct.month,ct.day,ct.hour,ct.minute,ct.second)
+    @created  = creat.to_i
+    mt  = row['Update_time']
+    modit = Time.gm(mt.year,mt.month,mt.day,mt.hour,mt.minute,mt.second)
+    @modified = modit.to_i
+    @comment  = row['Comment']
   end
-  
-  ### Close database connection
+end
+
+class Status_DB
+  attr_reader :size, :created, :modified, :name
+  def initialize(size,modified,created,name)
+    @size       = size
+    @modified   = modified
+    @created    = created
+    @name       = name
+  end
+end
+
+class MySQLAbstractor
+  def initialize(conf,db_name)
+    (@host,@user,@pass,@debe) = [conf[:host],conf[:user],conf[:pass],db_name]
+    @conn = false
+  end
   def open_conn
     begin
-      # Don't open if already open or no database specified
       if not @conn and @debe
-        # The second parameter is the mode, which is unused; use 0!
         @conn = DBI.connect("DBI:Mysql:#{@debe}:#{@host}",@user,@pass)
         return true
       end
     rescue DBI::DatabaseError
-      # simply reconnect on failure
-      @conn = DBI.connect("DBI:Mysql:#{@debe}:#{@host}",@user,@pass)
+      $stderr.write("Warn: DBConn.open_conn; unable to connect (@debe:#{@debe.inspect},@host:#{@host.inspect})\n")
+      @conn = DBI.connect("DBI:Mysql:mysql:#{@host}",@user,@pass)
+      db(@debe)
       return true
     end
   end
-  
-  ### Close database connection
   def close_conn
     begin
       @conn.disconnect if @conn # Close only when a connection is open
     rescue DBI::DatabaseError
+      $stderr.write("Note: DBConn.close_conn; unable to close connection (@debe:#{@debe.inspect},@host:#{@host.inspect})\n")
     end
     @conn = false
   end
-  
-  ### Return databases
   def dbs
+    open_conn if not @conn
     if @conn
-      databases = q("show databases")
-      return databases
+      databases = []
+      q("show databases").each do |database|
+        databases.push(database['Database'])
+      end
+      return databases.sort
     else
       return false
     end
   end
-  
-  ### Change/Create database
-  def db(nimi=false)
-    if nimi
-      luo_uusi = true
-      for kanta in dbs()
-        if kanta == nimi
-          luo_uusi = false
-        end
-      end
-      if luo_uusi == true
-        @conn.func(:createdb,nimi)
+  def tables
+    open_conn if not @conn
+    table_names = []
+    q("show tables").each do |table_name_hsh|
+      table_names.push(table_name_hsh[table_name_hsh.keys[0]])
+    end
+    return table_names.sort
+  end
+  def db(db_name=false)
+    if db_name
+      unless dbs.include?(db_name)
+        $stderr.write("Note: creating database #{db_name.inspect}\n")
+        #@conn.func(:createdb,db_name)
+        @conn.execute("create database #{db_name}")
       end
       close_conn
-      @debe = nimi
+      @debe = db_name
       open_conn
       return true
     else
       return false
     end
   end
-  
-  ### Perform a database query
-  def q(qu,ignore_cache=false)
-    get_id = false  # we are not inserting anything by default
-    
-    ## get the keyword of the query
+  def status
+    open_conn if not @conn
+    quo = @conn.execute("show table status")
+    status_tbl = {}
+    total_bytes = 0
+    modified_db = 0
+    created_db = 2**32
+    quo.fetch_hash do |row|
+      table_info = Status_TBL.new(row)
+      status_tbl[table_info.name] = table_info
+      modified_db = table_info.modified if modified_db < table_info.modified
+      created_db  = table_info.created  if created_db > table_info.created
+      total_bytes += table_info.size
+    end
+    status_db = Status_DB.new(total_bytes,modified_db,created_db,@debe)
+    return [status_db,status_tbl]
+  end
+  def q(qu)
     action = qu.split(' ')[0].downcase
-    
-    ## these are keywords that modify data
-    inserters = ['insert','update','delete','replace','create']
-    
-    ## check if we are going to change something
-    get_id = true if inserters.include?(action)
-    
-    ## open the connection if it's not opened earlier
+    inserters = ['insert']
+    updaters  = ['update','delete','replace','create','grant','drop']
+    get_id = inserters.include?(action)
+    get_count = updaters.include?(action)
     open_conn() if not @conn
-    
     if get_id
-      ## we are modifying something
       begin
-        @conn.execute(qu)
+        @conn.do(qu)
       rescue
-        ## if that fails, the probable cause was disconnection
-        ## by timeout, so lets reconnect:
         close_conn
         open_conn
-        @conn.execute(qu)
+        @conn.do(qu)
       end
-      ## get the new row id if it was created
       return @conn.func(:insert_id)
-      
+    elsif get_count
+      begin
+        return @conn.do(qu) # matched row count
+      rescue
+        close_conn
+        open_conn
+        return @conn.do(qu)
+      end
     elsif @debe
-      ## Well.. we are getting getting data.
       begin
         rows = []
         begin
           quo = @conn.execute(qu)
         rescue
+          $stderr.write("Warning: DBConn.q; query error, trying to reopen connection (qu.inspect)\n")
           close_conn
           open_conn
           quo = @conn.execute(qu)
         end
         quo.fetch_hash() do |row|
-          rows.push( row.clone ) if row != nil
+          rows.push( row ) if row != nil
         end
-        
-        ## return fetched data
-        return rows.clone
+        return rows
       end
     else
-      ## something failed
+      $stderr.write("ERROR: DBConn.q; unknown query type (qu.inspect)\n")
       return false
     end
   end
 end
-
