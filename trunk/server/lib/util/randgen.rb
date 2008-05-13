c373efddf678e63ccf9345db09c8849526c1b998
Juha-Jarmo Heinonen <o@rsence.org>
2008-05-13 17:43:26 +0000

    faster uuid generator
    git-svn-id: http://svn.rsence.org@178 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/server/lib/util/randgen.rb
+++ trunk/server/lib/util/randgen.rb
@@ -1,7 +1,6 @@
 ###
   # HIMLE RIA Server
   # Copyright (C) 2008 HIMLE GROUP http://himle.sorsacode.com/
-  # Copyright (C) 2006-2007 Helmi Technologies Inc.
   #  
   #  This program is free software; you can redistribute it and/or modify it under the terms
   #  of the GNU General Public License as published by the Free Software Foundation;
@@ -13,63 +12,51 @@
   #  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
   ###
 
-require 'digest/sha1'
+require 'md5'
 
 ## HRandgen generates a buffer of random strings and keeps that buffer
 ## Its give method always return a new unique number
 class RandomGenerator
   
+  attr_reader :stats
+  
   ## Initialize with:
   ##  - target_len: the target length of the string
   ##  - buffer_min: the minimum amount of strings in the buffer
-  def initialize( target_len, buffer_min )
-    @counter     = 0
-    @chars       = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-    @charcount   = @chars.size
+  def initialize( target_len, buffer_min, verbose=false )
+    @verbose     = verbose
+    if verbose
+      @stats = {:make=>0,:get=>0}
+    end
+    @counter     = rand(9223372036854775808)
     @buffer_min  = buffer_min
     @target_len  = target_len
     @buffer      = []
+    make( @buffer_min )
   end
   
   ## Generate one new string
   def generate
-    outp = []
-    @target_len.times do |n|
-      randnum = rand( @charcount )
-      outp.push( @chars[ randnum ].chr )
+    @counter += rand(32768)
+    @counter = rand(9223372036854775808) if @counter > 9223372036854775808
+    outp = [ MD5.digest(@counter.to_s) ]
+    (@target_len/20.0).ceil.times do |n|
+      outp.push( MD5.digest((@counter+rand(32768)).to_s) )
     end
-    return outp.join
+    return [outp.join].pack('m*').strip[0..(20+@target_len)]
   end
   
   ## Adds amount: number of keys to the buffer
   def make( amount = 1 )
-    
-    until amount == 0
-      
-      ## increment the counter by at least one
-      rand_add  = rand(9) + 1
-      @counter += rand_add
-      
-      ## flip the counter, when the number grows big enough
-      if @counter > 999999999999
-        @counter = rand_add
-      end
-      
-      ## uses the counter to prefix the random string to make sure that the 
-      prefix    = ''
-      Digest::SHA1.digest(@counter.to_s).each_byte do |digest_byte|
-        prefix += @chars[digest_byte%64].chr
-      end
-      generated = prefix + generate
-      unless @buffer.include?( generated )
-        @buffer.push( generated )
-        amount -= 1
-      end
+    @stats[:make]+=1 if @verbose
+    amount.times do |n|
+      @buffer.push( generate )
     end
   end
   
   ## Returns amount number of random strings packed in an array
   def get( amount = 1 )
+    @stats[:get]+=1 if @verbose
     make( @buffer_min + amount ) if @buffer.size < amount
     outp = []
     amount.times {
