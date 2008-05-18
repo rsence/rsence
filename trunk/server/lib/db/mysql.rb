331558d1966591b5db27deeac0a65f9f4513369f
Juha-Jarmo Heinonen <o@rsence.org>
2008-05-18 00:49:48 +0000

    upgraded license of the himle server to gpl v3.1
    git-svn-id: http://svn.rsence.org@185 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/server/lib/db/mysql.rb
+++ trunk/server/lib/db/mysql.rb
@@ -1,38 +1,27 @@
 ###
-  # HIMLE RIA Server
-  # Copyright (C) 2008 HIMLE GROUP http://himle.sorsacode.com/
-  #  
-  #  This program is free software; you can redistribute it and/or modify it under the terms
-  #  of the GNU General Public License as published by the Free Software Foundation;
-  #  either version 2 of the License, or (at your option) any later version. 
-  #  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-  #  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  #  See the GNU General Public License for more details. 
-  #  You should have received a copy of the GNU General Public License along with this program;
-  #  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+  # Himle Server -- http://himle.org/
+  #
+  # Copyright 2003, 2004, 2005, 2007, 2008 Juha-Jarmo Heinonen
+  #
+  # This file is part of Himle Server.
+  #
+  # Himle Server is free software: you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation, either version 3 of the License, or
+  # (at your option) any later version.
+  #
+  # Himle server is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+  #
+  # You should have received a copy of the GNU General Public License
+  # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  #
   ###
 
-#    This file is part of ROXOR, see http://roxor.sorsacode.com/
-
-#    ROXOR is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
-
-#    ROXOR is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#    Copyright 2003, 2004, 2005, 2007 Juha-Jarmo Heinonen
 
 ### ROXOR MySQL Abstractor
-##  by  Juha-Jarmo Heinonen (otus@olen.to)
-###
-
 
 require "dbi"
 
