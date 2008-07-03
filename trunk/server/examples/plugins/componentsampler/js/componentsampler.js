2a181b5319b30aa24361c06f1419c2aadb2a7212
Juha-Jarmo Heinonen <o@rsence.org>
2008-07-03 02:42:00 +0000

    componentsampler basics. unsurprisingly, most components are very outdated
    git-svn-id: http://svn.rsence.org@211 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/server/examples/plugins/componentsampler/js/componentsampler.js
+++ trunk/server/examples/plugins/componentsampler/js/componentsampler.js
@@ -8,9 +8,54 @@ ComponentSampler = HApplication.extend({
         label: 'Component Sampler'
       }
     );
-    this.win2 = new HWindow(
-      new HRect(10,10,100,100),
-      this
+    this.tabs = new (HTab.extend({
+        flexRight: true,
+        flexBottom: true,
+        flexRightOffset: 16,
+        flexBottomOffset: 16
+      }))(
+      new HRect(16,16,100,100),
+      this.window
+    );
+    this.buttonsTab = this.tabs.addTab('Buttons',true);
+    this.textTab = this.tabs.addTab('Text');
+    this.numericTab = this.tabs.addTab('Numeric');
+    this.progressTab = this.tabs.addTab('Progress');
+    this.mediaTab = this.tabs.addTab('Media');
+    this.populateButtons();
+  },
+  populateButtons: function(){
+    var _buttonRect = new HRect(8,8,108,28);
+    this.button1 = new HButton(
+      new HRect(_buttonRect),
+      this.buttonsTab, {
+        label: 'HButton enabled'
+      }
+    );
+    _buttonRect.offsetBy(160,0);
+    this.button2 = new HButton(
+      new HRect(_buttonRect),
+      this.buttonsTab, {
+        label: 'HButton disabled',
+        enabled: false
+      }
+    );
+    _buttonRect.offsetBy(-160,30);
+    this.button3 = new HCheckbox(
+      new HRect(_buttonRect),
+      this.buttonsTab, {
+        label: 'HCheckbox',
+        value: true
+      }
+    );
+    _buttonRect.offsetBy(160,0);
+    this.button2 = new HCheckbox(
+      new HRect(_buttonRect),
+      this.buttonsTab, {
+        label: 'HCheckbox disabled',
+        enabled: false,
+        value: false
+      }
     );
   }
 });
