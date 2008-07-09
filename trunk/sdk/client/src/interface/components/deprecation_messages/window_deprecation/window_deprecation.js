35a549821af99d3f8e97e30252dadce4c2140c58
Juha-Jarmo Heinonen <o@rsence.org>
2008-07-09 14:52:12 +0000

    tabs.js and window.js are deprecated, because the components are included in basic.js
    git-svn-id: http://svn.rsence.org@223 5bb182ed-6ace-4cc9-85f1-9567b96542ab

---
+++ trunk/sdk/client/src/interface/components/deprecation_messages/window_deprecation/window_deprecation.js
@@ -0,0 +1,7 @@
+
+if(console){
+  if(console.log){
+    console.log('HimleDeprecationWarning',"Don't include window.js separately anymore. It's included in basic.js and window.js won't be present for much longer.");
+  }
+}
+
