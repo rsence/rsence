896fb6636a67268fdafb9096b0f689d35536614f
Juha-Jarmo Heinonen <o@rsence.org>
2008-08-25 15:53:50 +0000

    minimal foundation for new value type added. keyvalues are going to be a database-ish way to handle and sync complex data structures (key/value hashes and index/value arrays)
    git-svn-id: http://svn.rsence.org@276 5bb182ed-6ace-4cc9-85f1-9567b96542ab

---
+++ trunk/server/lib/values/keyvalue.rb
@@ -0,0 +1,38 @@
+# -* coding: UTF-8 -*-
+###
+  # Himle Server -- http://himle.org/
+  #
+  # Copyright (C) 2008 Juha-Jarmo Heinonen
+  # Copyright (C) 2006-2007 Helmi Technologies Inc.
+  #
+  # This file is part of Himle Server.
+  #
+  # Himle Server is free software: you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation, either version 3 of the License, or
+  # (at your option) any later version.
+  #
+  # Himle server is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+  #
+  # You should have received a copy of the GNU General Public License
+  # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  #
+  ###
+
+module Himle
+module Server
+
+### KeyValues are (going to be) values that can exchange
+##  array/hash -like objects with partial syncing abilities.
+## 
+##  This will allow complex data structures in the client with less kludging.
+##
+###
+class KeyValue
+end
+
+end
+end
