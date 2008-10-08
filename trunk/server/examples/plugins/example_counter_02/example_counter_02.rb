011b1eff5454a66d60619dc57fc065354b9c9dcb
Juha-Jarmo Heinonen <o@rsence.org>
2008-10-08 01:36:45 +0000

    example_counter_02 does the same thing as example_counter_01 except it also saves its data
    git-svn-id: http://svn.rsence.org@322 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/server/examples/plugins/example_counter_02/example_counter_02.rb
+++ trunk/server/examples/plugins/example_counter_02/example_counter_02.rb
@@ -1,6 +1,7 @@
 
-##### CounterPlugin example, extends the default Plugin class. Gathers statistics.
-class CounterPlugin01 < Plugin
+##### CounterPlugin example, extends the default Plugin class.
+  ### Gathers statistics, stores statistics.
+class CounterPlugin02 < Plugin
   
   ### Called after primary initialization, sets up a few counters.
   def init
@@ -35,8 +36,22 @@ class CounterPlugin01 < Plugin
     msg.reply "console.log('Hit count: #{@hits_total}');"
   end
   
+  ### Called, when it's time to save changes.
+  def flush
+    file_save( 'data/hits_total',    @hits_total.to_s   )
+    file_save( 'data/visits_total',  @visits_total.to_s )
+    file_save( 'data/visits_unique', @visits_uniqu.to_s )
+  end
+  
+  ### Called, when it's time to read data
+  def open
+    @hits_total   = ( file_read( 'data/hits_total'    ) or 0 ).to_i
+    @visits_total = ( file_read( 'data/visits_total'  ) or 0 ).to_i
+    @visits_uniqu = ( file_read( 'data/visits_unique' ) or 0 ).to_i
+  end
+  
 end
 
-# Creates the instance and registers as example_counter_01
-CounterPlugin01.new.register 'example_counter_01'
+# Creates the instance and registers as example_counter_02
+CounterPlugin02.new.register 'example_counter_02'
 
