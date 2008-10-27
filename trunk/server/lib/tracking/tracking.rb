77d21733b616f644318246d838fc88ac1d75e52e
Juha-Jarmo Heinonen <o@rsence.org>
2008-10-27 13:27:49 +0000

    setup_mysql method for tracking added
    git-svn-id: http://svn.rsence.org@330 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/server/lib/tracking/tracking.rb
+++ trunk/server/lib/tracking/tracking.rb
@@ -24,8 +24,6 @@ module Tracking
         #  action_id        The row id of the action, relative to the table used
         #  time_started_ms  The time (epoch milliseconds when the action started)
         #  time_elapsed_ms  The time taken for the action (in milliseconds)
-        #  data             Custom data for action
-        #  data_bytes       Size of custom data for action
         #  cpu_load_x_100   CPU load at the end of the action, multiplied by 100
         #
         @db.q( %{
@@ -50,9 +48,11 @@ module Tracking
         #  Column descriptions of himle_tracking
         #
         #  Name         Description
+        #
         #  id           The primary row id: unique integer, same as himle_tracking: action_type
         #  description  Human-readable description of action
         #  action_table Table name of the action
+        #
         @db.q( %{
           create table himle_tracking_actions (
             id           int primary key unique,
@@ -65,6 +65,28 @@ module Tracking
     end
     
     def setup_mysql
+      unless @db.tables.include?('himle_tracking_mysql')
+        puts "Creating mysql actions table"
+        
+        ### Action description table for tracking mode
+        #
+        #  Column descriptions of himle_tracking
+        #
+        #  Name         Description
+        #
+        #  id           The primary row id: unique integer, same as himle_tracking: action_type
+        #  description  Human-readable description of action
+        #  action_table Table name of the action
+        #
+        @db.q( %{
+          create table himle_tracking_mysql (
+            id           int primary key unique,
+            mysql_method int not null default 0,
+            query        text,
+            result       mediumtext
+          ) default charset utf8
+        }.gsub("\n",' ').squeeze(' ') )
+      end
     end
     
     def setup_pluginamanager
