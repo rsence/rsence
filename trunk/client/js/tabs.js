2cfc220cb2cd2f92e2c4268c52b8de8838a40ce1
Juha-Jarmo Heinonen <o@rsence.org>
2008-11-17 06:16:00 +0000

    new client build
    git-svn-id: http://svn.rsence.org@344 5bb182ed-6ace-4cc9-85f1-9567b96542ab

---
+++ trunk/client/js/tabs.js
@@ -0,0 +1,2 @@
+
+if(console){if(console.log){console.log('HimleDeprecationWarning',"Don't include tabs.js separately anymore. It's included in basic.js and window.js won't be present for much longer.");}}
