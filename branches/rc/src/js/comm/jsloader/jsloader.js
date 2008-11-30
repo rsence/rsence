9f40b468181431a5c4a13d1fa2a2cd7c45eb506f
Juha-Jarmo Heinonen <o@rsence.org>
2008-11-30 21:08:33 +0000

    Cleanup and prevention for double response handling on (at least) safari.
    git-svn-id: http://svn.rsence.org@372 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/rc/src/js/comm/jsloader/jsloader.js
+++ branches/rc/src/js/comm/jsloader/jsloader.js
@@ -17,38 +17,40 @@
 JSLoader = HClass.extend({
   
   constructor: function(_uri){
-    this._loadedJS = [];
-    this.uri  = _uri;
-    this._req = null;
-    //this._currJS = [];
+    var _this = this;
+    _this._loadedJS = [];
+    _this.uri  = _uri;
+    _this._req = null;
+    _this._okayed = false;
   },
   
   _okay: function(_resp){
-    //var _loadedJS = jsLoader._currJS.shift();
-    //console.log('resp: ',_resp);
-    //console.log('loadedJS: ',_loadedJS);
-    //console.log('jsLoader.loadedJS: ',jsLoader._loadedJS);
-    //console.log('jsLoader.currJS: ',jsLoader._currJS);
+    var _this = this;
+    if(_this._okayed){
+      return;
+    }
     eval(_resp.responseText);
+    _this._okayed = true;
   }, 
   
   load: function(_jsName,_fullURL){
-    if((jsLoader._loadedJS.indexOf(_jsName)!=-1) && (_fullURL === undefined)) {
+    var _this = this;
+    if((_this._loadedJS.indexOf(_jsName)!=-1) && (_fullURL === undefined)) {
       return;
     }
-
     if (_fullURL) {
-      document.write('<script type="text/javascript" src="'+_jsName+'"><'+'/script>');
+      document.write('<scr'+'ipt type="text/javascript" src="'+_jsName+'"><'+'/scr'+'ipt>');
     } else {
-      req_args = {
-        onSuccess:    function(resp){jsLoader._okay(resp);},
-        onFailure:    function(resp){window.status="failed to load js: "+jsLoader._currJS;},
+      var _req_args = {
+        onSuccess:    function(resp){_this._okay(resp);},
+        onFailure:    function(resp){window.status="failed to load js: "+_this.uri+_jsName+'.js';},
         method:       'get',
         asynchronous: false
       };
       var _url = this.uri+_jsName+'.js';
-      this._req = new Ajax.Request( _url, req_args );
-      this._loadedJS.push(_jsName);
+      _this._req = new Ajax.Request( _url, _req_args );
+      _this._loadedJS.push(_jsName);
+      _this._okayed = false;
     }
   }
   
