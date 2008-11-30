8d4f7c8cc0813dcd27ca2749015cc5a573ac4674
Juha-Jarmo Heinonen <o@rsence.org>
2008-11-30 00:51:08 +0000

    removing stuff not used any more, going to fix the rest
    git-svn-id: http://svn.rsence.org@356 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/rc/src/js/ui/controls/deprecation_messages/tab_deprecation/tab_deprecation.js
+++
@@ -1,7 +0,0 @@
-
-if(console){
-  if(console.log){
-    console.log('HimleDeprecationWarning',"Don't include tabs.js separately anymore. It's included in basic.js and window.js won't be present for much longer.");
-  }
-}
-
