6406fe4aa687ada5b8a08466e2ebc51c45c339ea
Juha-Jarmo Heinonen <o@rsence.org>
2009-01-05 03:02:45 +0000

    improved the numeric tab layout and functionality
    git-svn-id: http://svn.rsence.org@419 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/rc/plugins/componentsampler/js/modules/sampler_numeric.js
+++ branches/rc/plugins/componentsampler/js/modules/sampler_numeric.js
@@ -2,8 +2,9 @@
 HimleSampler.SamplerNumeric = {
   createNumericTabContents: function(){
     
+    var _helperRect = HRect.nu( 8,8 , 29,263 );
     this.sliderVertical = HVSlider.nu(
-      HRect.nu( 8,8 , 29,263 ),
+      HRect.nu( _helperRect ),
       this.numericTab, {
         minValue: -100,
         maxValue: 100
@@ -11,8 +12,9 @@ HimleSampler.SamplerNumeric = {
     );
     this.values.num1.bind( this.sliderVertical );
     
+    _helperRect.offsetBy( 32, 0 );
     this.sliderVerticalDisabled = HVSlider.nu(
-      HRect.nu( 37,8 , 58,263 ),
+      HRect.nu( _helperRect ),
       this.numericTab, {
         enabled: false,
         minValue: -100,
@@ -21,8 +23,9 @@ HimleSampler.SamplerNumeric = {
     );
     this.values.num1.bind( this.sliderVerticalDisabled );
     
+    _helperRect.set( 70,8 , 325,29 );
     this.sliderHorizonal = HSlider.nu(
-      HRect.nu( 70,8 , 325,29 ),
+      HRect.nu( _helperRect ),
       this.numericTab, {
         minValue: -100,
         maxValue: 100
@@ -30,8 +33,9 @@ HimleSampler.SamplerNumeric = {
     );
     this.values.num1.bind( this.sliderHorizonal );
     
+    _helperRect.offsetBy( 0, 32 );
     this.sliderHorizonalDisabled = HSlider.nu(
-      HRect.nu( 70,37 , 325,58 ),
+      HRect.nu( _helperRect ),
       this.numericTab, {
         enabled: false,
         minValue: -100,
@@ -40,17 +44,42 @@ HimleSampler.SamplerNumeric = {
     );
     this.values.num1.bind( this.sliderHorizonalDisabled );
     
-    this.num1StringView = HStringView.nu(
-      HRect.nu( 333, 8, 400, 29 ),
-      this.numericTab
+    _helperRect.offsetBy( 0, 32 );
+    _helperRect.setRight( _helperRect.right - 15 );
+    this.num1StringEdit = HTextControl.extend({
+      setValue: function(_value){
+        try {
+          var _pow = Math.pow( 10, this.options.decimals );
+          _value = parseInt( parseFloat( _value ) * _pow, 10) / _pow;
+        }
+        catch(e) {
+          _value = 0;
+        }
+        this.base( _value );
+      }
+    }).nu(
+      HRect.nu( _helperRect ),
+      this.numericTab, {
+        events: {
+          textEnter: true
+        },
+        decimals: 3
+      }
     );
-    this.values.num1.bind( this.num1StringView );
+    this.values.num1.bind( this.num1StringEdit );
     
+    _helperRect.setLeftTop( _helperRect.rightTop );
+    _helperRect.setWidth( 15 );
     this.num1Stepper = HStepper.nu(
-      HRect.nu( 333, 37, 400, 58 ),
-      this.numericTab
+      HRect.nu( _helperRect ),
+      this.numericTab, {
+        minValue: -100,
+        maxValue: 100,
+        repeatInterval: 50
+      }
     );
     this.values.num1.bind( this.num1Stepper );
+    
     
   }
 };
