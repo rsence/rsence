129c1da6c9efd1162c6dd53917773525fb201788
Juha-Jarmo Heinonen <o@rsence.org>
2009-01-22 01:44:17 +0000

    reordered url_matching to match plugins first (experimental)
    git-svn-id: http://svn.rsence.org@452 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/rc/lib/http/broker.rb
+++ branches/rc/lib/http/broker.rb
@@ -46,32 +46,33 @@ class Broker
     ## User agent:
     ua = @request.header['user-agent'] if $DEBUG_MODE
     
-    ## /x handles xhr without cookies
-    if uri == '/x'
-      puts "/x: #{uri.inspect}" if $DEBUG_MODE
-      $TRANSPORTER.xhr( @request, @response, false )
+    uri_matched = $TRANSPORTER.servlet( :post, @request, @response )
     
-    ## /hello handles the first xhr (with cookies, for session key)
-    elsif uri == '/hello'
-      puts "/hello: #{uri.inspect}" if $DEBUG_MODE
-      $TRANSPORTER.xhr( @request, @response, true )
-    
-    ## /SOAP handles SOAP Requests
-    elsif uri == '/SOAP'
-      puts "/SOAP: #{uri.inspect}"
-      $TRANSPORTER.soap( @request, @response )
-    
-    ## /up handles the uploads inited for ticketserve
-    ## the second part of the uri contains the disposable
-    ## upload key that has a server-side mapping to the
-    ## user's session
-    elsif uri[0..2] == '/U/'
-      puts "/U: #{uri.inspect}" if $DEBUG_MODE
-      $TICKETSERVE.upload( @request, @response )
-    ## servlet matching
-    else
-      uri_matched = $TRANSPORTER.servlet( :post, @request, @response )
-      unless uri_matched
+    unless uri_matched
+      ## /x handles xhr without cookies
+      if uri == '/x'
+        puts "/x: #{uri.inspect}" if $DEBUG_MODE
+        $TRANSPORTER.xhr( @request, @response, false )
+  
+      ## /hello handles the first xhr (with cookies, for session key)
+      elsif uri == '/hello'
+        puts "/hello: #{uri.inspect}" if $DEBUG_MODE
+        $TRANSPORTER.xhr( @request, @response, true )
+  
+      ## /SOAP handles SOAP Requests
+      elsif uri == '/SOAP'
+        puts "/SOAP: #{uri.inspect}"
+        $TRANSPORTER.soap( @request, @response )
+  
+      ## /up handles the uploads inited for ticketserve
+      ## the second part of the uri contains the disposable
+      ## upload key that has a server-side mapping to the
+      ## user's session
+      elsif uri[0..2] == '/U/'
+        puts "/U: #{uri.inspect}" if $DEBUG_MODE
+        $TICKETSERVE.upload( @request, @response )
+      ## servlet matching
+      else
         puts "/404: #{uri.inspect}" if $DEBUG_MODE
         @response.status = 404
         err404 = '<html><head><title>404 - Page Not Found</title></head><body>404 - Page Not Found</body></html>'
@@ -92,48 +93,49 @@ class Broker
     ## User agent:
     ua = @request.header['user-agent'] if $DEBUG_MODE
     
-    ## /j processes client framework files (js & themes)
-    if uri[0..2] == '/H/'
-      puts "/H: #{uri.inspect}" if $DEBUG_MODE
-      $FILESERVE.get( @request, @response )
+    uri_matched = $TRANSPORTER.servlet( :get, @request, @response )
     
-    ## /i returns disposable RMagick objects rendered to data
-    elsif uri[0..2] == '/i/'
-      puts "/i: #{uri.inspect}" if $DEBUG_MODE
-      $TICKETSERVE.get( @request, @response, :img )
+    unless uri_matched
+      ## /j processes client framework files (js & themes)
+      if uri[0..2] == '/H/'
+        puts "/H: #{uri.inspect}" if $DEBUG_MODE
+        $FILESERVE.get( @request, @response )
     
-    ## /d returns static data resources
-    elsif uri[0..2] == '/d/'
-      puts "/d: #{uri.inspect}" if $DEBUG_MODE
-      $TICKETSERVE.get( @request, @response, :rsrc )
+      ## /i returns disposable RMagick objects rendered to data
+      elsif uri[0..2] == '/i/'
+        puts "/i: #{uri.inspect}" if $DEBUG_MODE
+        $TICKETSERVE.get( @request, @response, :img )
     
-    ## /f return disposable data resources
-    elsif uri[0..2] == '/f/'
-      puts "/f: #{uri.inspect}" if $DEBUG_MODE
-      $TICKETSERVE.get( @request, @response, :file )
+      ## /d returns static data resources
+      elsif uri[0..2] == '/d/'
+        puts "/d: #{uri.inspect}" if $DEBUG_MODE
+        $TICKETSERVE.get( @request, @response, :rsrc )
     
-    ## /B return smart data objects
-    elsif uri[0..2] == '/b/'
-      puts "/b: #{uri.inspect}" if $DEBUG_MODE
-      $TICKETSERVE.get( @request, @response, :blobobj )
+      ## /f return disposable data resources
+      elsif uri[0..2] == '/f/'
+        puts "/f: #{uri.inspect}" if $DEBUG_MODE
+        $TICKETSERVE.get( @request, @response, :file )
     
-    ## special case for favicon
-    elsif uri == '/favicon.ico'
-      $TICKETSERVE.favicon( @request, @response )
+      ## /B return smart data objects
+      elsif uri[0..2] == '/b/'
+        puts "/b: #{uri.inspect}" if $DEBUG_MODE
+        $TICKETSERVE.get( @request, @response, :blobobj )
     
-    ## empty html page for the uploader iframe
-    elsif uri == '/U/iframe_html'
-      puts "/U/iframe_html: #{uri.inspect}" if $DEBUG_MODE
-      @response.status = 200
-      http_body = '<html><head><title>Empty Iframe for Uploading</title></head><body></body></html>'
-      @response['content-type'] = 'text/html; charset=UTF-8'
-      @response['content-length'] = http_body.size.to_s
-      @response.body = http_body
+      ## special case for favicon
+      elsif uri == '/favicon.ico'
+        $TICKETSERVE.favicon( @request, @response )
     
-    ## servlet matching
-    else
-      uri_matched = $TRANSPORTER.servlet( :get, @request, @response )
-      unless uri_matched
+      ## empty html page for the uploader iframe
+      elsif uri == '/U/iframe_html'
+        puts "/U/iframe_html: #{uri.inspect}" if $DEBUG_MODE
+        @response.status = 200
+        http_body = '<html><head><title>Empty Iframe for Uploading</title></head><body></body></html>'
+        @response['content-type'] = 'text/html; charset=UTF-8'
+        @response['content-length'] = http_body.size.to_s
+        @response.body = http_body
+    
+      ## servlet matching
+      else
         puts "/404: #{uri.inspect}" if $DEBUG_MODE
         @response.status = 404
         err404 = '<html><head><title>404 - Page Not Found</title></head><body>404 - Page Not Found</body></html>'
