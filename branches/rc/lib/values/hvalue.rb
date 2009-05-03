6f0ac80fbf09d1754e7a9e8f24549255da399743
Juha-Jarmo Heinonen <o@rsence.org>
2009-05-03 23:55:47 +0000

    minor cleanup, proper destructor, UploadValue stub (incomplete)
    git-svn-id: http://svn.rsence.org@509 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/rc/lib/values/hvalue.rb
+++ branches/rc/lib/values/hvalue.rb
@@ -67,7 +67,7 @@ class HValue
   ## (Re-)Send the client-size representation
   def restore( msg )
     ## Initialize a new client value
-    init_str = "new HValue(#{@val_id.inspect}, #{@data.to_json});"
+    init_str = "HValue.nu('#{@val_id}', #{@data.to_json});"
     msg.reply( init_str )
   end
   
@@ -195,13 +195,151 @@ class HValue
   
   ## tell the client that the value changed
   def to_client( msg )
-    msg.reply "HVM.s( #{@val_id.inspect}, #{@data.to_json} );" 
+    msg.reply "HVM.s( '#{@val_id}', #{@data.to_json} );" 
   end
   
   ## clean up self
-  def die
+  def die( msg=false )
+    
+    release_all
+    
+    # get the value storage from the session data
+    session_values = msg.session[:values][:by_id]
+    
+    ## Store the object here
+    session_values.delete[ @val_id ]
+    
+    if msg
+      msg.reply("HVM.del('#{@val_id}');")
+    end
   end
   
+end
+
+class UploadValue < HValue
+  
+  @state_responders = {
+    :ready   => [], # id == 0
+    :started => [], # id == 1
+    :process => [], # id == 2 ; also uses bind
+    3 => [], # id == 3
+    4 => [], # id == 4
+    :error => [] # id < 0
+  }
+  
+  @upload_state = 0
+  @upload_key = ''
+  @uploads = []
+  
+  # the data should contain both state and key in the value
+  def from_client( msg, data )
+    
+    ## change the valid state, because the value was set by the client!
+    @is_valid = data.include?(':::')
+    
+    # the state and key are separated by the ':::' delimitter string
+    if @is_valid
+      
+      # split state and key using the delimitter
+      (upload_state, upload_key) = data.split(':::')
+      
+      # the state is a number
+      upload_state = upload_state.to_i
+      
+      @upload_state = upload_state
+      @upload_key = upload_key
+      
+      # negative states are errors
+      if upload_state < 0
+        # "upload error: #{upload_state}"
+        # (parse the error)
+        unless @state_respders[:error].empty?
+          @state_responders[:error].each do |plugin_name,method_name|
+            msg.run( plugin_name,method_name,msg,self,upload_state )
+          end
+        end
+        
+      # the default state, 0 means the ui is ready to send an
+      # upload and ticketserve is ready to receive it
+      elsif upload_state == 0
+        # "upload state: ready to upload."
+        # (do nothing)
+        
+        unless @state_respders[:ready].empty?
+          @state_responders[:ready].each do |plugin_name,method_name|
+            msg.run( plugin_name,method_name,msg,self,upload_state )
+          end
+        end
+        
+      # this state means the upload's transfer is started and progressing
+      elsif upload_state == 1
+        # "upload state: upload started."
+        # (show progress bar)
+        
+        unless @state_respders[:started].empty?
+          @state_responders[:started].each do |plugin_name,method_name|
+            msg.run( plugin_name,method_name,msg,self,upload_state )
+          end
+        end
+      
+      # this state means the upload's transfer is complete,
+      # but the uploaded data hasn't been handled yet.
+      elsif upload_state == 2
+        # "upload state: waiting to process."
+        
+        uploads = $TICKETSERVE.get_uploads(upload_key,true)
+        if uploads.size == 1
+          uploaded_data = uploads[0]
+          
+          # only process changes, if different from the one already stored.
+          if uploaded_data != @data
+            
+            @data = uploaded_data
+            
+            ## add the id to the values to be checked
+            check_ids = msg.session[:values][:check]
+            unless check_ids.include?( @val_id )
+              check_ids.push( @val_id )
+            end
+            
+          end
+          $TICKETSERVE.del_uploads(upload_key,msg.ses_id)
+        else
+          # "upload, amount of uploads: #{uploads.size}"
+        end
+        
+        # 
+        hvalue.set(msg,"3:::#{upload_key}")
+        
+        msg.console( "upload state: set to ack" )
+        
+      elsif upload_state == 3
+        # "upload state: waiting for user ack."
+        # (do nothing)
+        
+        msg.console( "upload state: waiting user ack" )
+        
+        
+      elsif upload_state == 4
+        # "upload state: user wants to upload again."
+        # (set a new upload key, )
+        
+        msg.console( "upload state: ack, getting new key" )
+        
+        
+        setup_upload( msg, hvalue )
+        
+        
+      else
+        # "upload unknown state: #{upload_state.inspect}"
+      end
+    end
+    return true
+  end
+  def setup_upload(msg,hvalue,size_bytes=500*1024,accept_mime=/image\/(.*?)/,allow_multi=false)
+    upload_key = $TICKETSERVE.upload_key(msg,hvalue.val_id,size_bytes,accept_mime,allow_multi)
+    hvalue.set( msg, upload_key )
+  end
 end
 
 ## Skeleton HValue parser
