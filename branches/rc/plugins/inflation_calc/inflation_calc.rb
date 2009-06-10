0b8d7d984b5181a46a876704de6d38a0deb96e10
Juha-Jarmo Heinonen <o@rsence.org>
2009-06-10 06:36:15 +0000

    git-svn-id: http://svn.rsence.org@549 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/rc/plugins/inflation_calc/inflation_calc.rb
+++ branches/rc/plugins/inflation_calc/inflation_calc.rb
@@ -1,70 +1,24 @@
+$LOAD_PATH << PluginManager.curr_plugin_path
+require 'lib/inflation_calc_responder'
 class InflationCalc < Plugin
-  def calculate( msg, value=nil )
-    ses   = msg.session[:inflation]
-    percent = ses[:percent].data.to_f
-    money = ses[:money].data.to_i
-    years = ses[:years].data.to_i
-    money_max =  100_000_000_000_000
-    money_min = -100_000_000_000_000
-    if money < money_min
-      money = money_min
-      ses[:money].set(msg,money)
-    elsif money > money_max
-      money = money_max
-      ses[:money].set(msg,money)
-    end
-    years_min = 1
-    years_max = 100
-    if years < years_min
-      years = years_min
-      ses[:years].set(msg,years)
-    elsif years > years_max
-      years = years_max
-      ses[:years].set(msg,years)
-    end
-    percent_min = -20
-    percent_max = 20
-    if percent > percent_max
-      percent = percent_max
-      ses[:percent].set(msg,percent)
-    end
-    if percent < percent_min
-      percent = percent_min
-      ses[:percent].set(msg,percent)
-    end
-    rate = 1-(percent*0.01)
-    result_future = []
-    result_past   = []
-    calc_future = money
-    calc_past   = money
-    years.times do |y|
-      calc_future *= rate
-      calc_past   /= rate
-      result_future.push( calc_future.round )
-      result_past.push(     calc_past.round )
-    end
-    puts result_past.inspect
-    puts result_future.inspect
-    ses[:result_past  ].set( msg, result_past   )
-    ses[:result_future].set( msg, result_future )
-    return true
-  end
+  include InflationCalcResponder
   def init_ses( msg )
     msg.session[:inflation] = {
       :percent =>  HValue.new(msg, 7 ),
-      :money   =>  HValue.new(msg, 1000 ),
+      :amount  =>  HValue.new(msg, 1000 ),
       :years   =>  HValue.new(msg, 10 ),
       :result_future =>  HValue.new(msg, [] ),
       :result_past   =>  HValue.new(msg, [] )
     }
     ses = msg.session[:inflation]
-    [ :percent, :money, :years ].each do |val|
+    [ :percent, :amount, :years ].each do |val|
       ses[val].bind('inflation','calculate')
     end
   end
   def init_ui( msg )
     ses = msg.session[:inflation]
-    include_js( msg, ['controls'] )
+    include_js( msg, ['controls','default_theme'] )
+    msg.reply require_js( 'simple_table' )
     msg.reply require_js( 'inflation_calc' )
     calculate( msg )
     msg.reply "inflationCalc = InflationCalc.nu(#{extract_hvalues_from_hash(ses)});"
--- branches/rc/plugins/inflation_calc/js/inflation_calc.js
+++ branches/rc/plugins/inflation_calc/js/inflation_calc.js
@@ -1,83 +1,48 @@
 InflationCalc = HApplication.extend({
+  reset: function(){
+    var values = this.values;
+    values.amount.set(1000);
+    values.years.set(10);
+    values.percent.set(7);
+  },
   constructor: function(values){
     this.values = values;
     this.base();
-    this.view = HView.extend({
-      flexRight: true, flexBottom: true,
+    this.view = HWindow.extend({
       drawSubviews: function(){
-        this.setStyle('background-color','#ccc');
-        HStringView.nu( HRect.nu(80,20,400,120),this,{value:'<h1>Inflation calculator</h1>'});
-        HStringView.nu( HRect.nu(80,122,220,145),this,{value:'<b>Current value:</b>'});
-        HTextControl.nu(HRect.nu(220,120,330,140),this,{valueObj:this.app.values.money,events:{textEnter:true}});
-        HStringView.nu( HRect.nu(80,152,220,175),this,{value:'<b>Inflation rate:</b>'});
-        HTextControl.nu(HRect.nu(220,150,260,170),this,{valueObj:this.app.values.percent,events:{textEnter:true}});
-        HStringView.nu( HRect.nu(260,152,280,175),this,{value:'%'});
+        HStringView.nu( HRect.nu(8,22,150,45),this,{value:'<b>Current value:</b>'});
+        HTextControl.nu(HRect.nu(150,20,250,40),this,{valueObj:this.app.values.amount,events:{textEnter:true}});
+        HButton.extend({
+          click: function(){
+            this.app.reset();
+          }
+        }).nu(HRect.nu(258,18,330,42),this,{events:{click:true},label:'Reset'});
+        HStringView.nu( HRect.nu(8,52,150,75),this,{value:'<b>Inflation rate:</b>'});
+        HTextControl.nu(HRect.nu(150,50,190,70),this,{valueObj:this.app.values.percent,events:{textEnter:true}});
+        HStringView.nu( HRect.nu(190,52,210,75),this,{value:'%'});
         HSlider.extend({
           setValue: function(value){
             if(isNaN(value)){return;}
-            this.base(Math.round(value*10)/10);
+            this.base(Math.round(value*10)/10); // makes decimals with just one digit after the dot
           }
-        }).nu(HRect.nu(270,150,410,170),this,{valueObj:this.app.values.percent,minValue:-20,maxValue:20});
-        HStringView.nu( HRect.nu(80,182,220,205),this,{value:'<b>Number of years:</b>'});
-        HTextControl.nu(HRect.nu(220,180,260,200),this,{valueObj:this.app.values.years,events:{textEnter:true}});
-        HStringView.nu( HRect.nu(80,220,200,240),this,{value:'<b>Future value:</b>'});
-        HStringView.nu( HRect.nu(250,220,400,240),this,{value:'<b>Past value:</b>'});
-        var SimpleTable = HControl.extend({
-          flexBottom: true, flexBottomOffset: 8,
-          drawSubviews: function(){
-            this.setStyle('background-color','#eee');
-            this.setStyle('overflow','auto');
-            this.setStyle('overflow-y','scroll');
-            this.setStyle('border','1px solid #999');
-            this.setStyle('font-size','11px');
-            this.setStyle('font-family','arial,sans-serif');
-            this.setStyle('text-align','right');
-            this.setStyle('line-height','20px');
-            this.setStyle('vertical-align','middle');
-          },
-          leftColStyle: 'position:absolute;display:block;left:0px;width:30px;padding-right:8px;height:20px;',
-          rightColStyle: 'position:absolute;overflow:hidden;text-overflow:ellipsis;display:block;border-left:1px solid #ccc;left:38px;padding-right:6px;width:90px;height:20px;',
-          refreshTableElems: function(){
-            if(this['tableElems']===undefined){
-              this.tableElems = [];
-            }
-            var tableElemsLen = this.tableElems.length,
-                valueLen = this.value.length,
-                maxLen = Math.max(tableElemsLen,valueLen),
-                elemId_col1, elemId_col2, row, i=0;
-            for(;i<maxLen;i++){
-              if(i >= tableElemsLen){
-                elemId_col1 = ELEM.make(this.elemId);
-                ELEM.setCSS(elemId_col1,this.leftColStyle);
-                elemId_col2 = ELEM.make(this.elemId);
-                ELEM.setCSS(elemId_col2,this.rightColStyle);
-                row = [elemId_col1,elemId_col2];
-                this.tableElems[i] = row;
-              }
-              else if(i>=valueLen){
-                var row = this.tableElems.pop();
-                ELEM.del(row[0]);
-                ELEM.del(row[1]);
-              }
-            }
-          },
-          refreshValue: function(){
-            this.refreshTableElems();
-            var row, i=0, colors=['#eee','#ddd'];
-            for(;i<this.value.length;i++){
-              row = this.tableElems[i];
-              ELEM.setStyle(row[0],'top',(i*20)+'px');
-              ELEM.setStyle(row[1],'top',(i*20)+'px');
-              ELEM.setStyle(row[0],'background-color',colors[i%2]);
-              ELEM.setStyle(row[1],'background-color',colors[i%2]);
-              ELEM.setHTML(row[0],i+1);
-              ELEM.setHTML(row[1],this.value[i]);
-            }
+        }).nu(HRect.nu(200,50,330,70),this,{valueObj:this.app.values.percent,minValue:-20,maxValue:20});
+        HStringView.nu( HRect.nu(8,82,150,105),this,{value:'<b>Number of years:</b>'});
+        HTextControl.nu(HRect.nu(150,80,190,100),this,{valueObj:this.app.values.years,events:{textEnter:true}});
+        HSlider.extend({
+          setValue: function(value){
+            if(isNaN(value)){return;}
+            this.base(Math.round(value)); // makes integers
           }
-        });
-        SimpleTable.nu(HRect.nu( 80,240,230,500),this,{valueObj:this.app.values.result_future});
-        SimpleTable.nu(HRect.nu(250,240,400,500),this,{valueObj:this.app.values.result_past  });
+        }).nu(HRect.nu(200,80,330,100),this,{valueObj:this.app.values.years,minValue:1,maxValue:100});
+        HStringView.nu( HRect.nu(8,120,158,140),this,{value:'<b>Future value:</b>'});
+        HStringView.nu( HRect.nu(180,120,330,140),this,{value:'<b>Past value:</b>'});
+        SimpleTable.nu(HRect.nu( 8,140,158,300),this,{valueObj:this.app.values.result_future});
+        SimpleTable.nu(HRect.nu(180,140,330,300),this,{valueObj:this.app.values.result_past  });
       }
-    }).nu(HRect.nu(0,0,408,508),this);
+    }).nu(HRect.nu(40,40,386,508),this,{
+      label: 'Inflation calculator',
+      minSize: [346,380],
+      maxSize: [346,3072]
+    });
   }
 });
