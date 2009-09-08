d8e08f7fdce3e6aa4593934c165883be33150ed9
Toni Nurminen <toni.nurminen@rsence.org>
2009-09-08 18:56:49 +0000

    General file system mounter. Definitely work in progress. Includes readme for dependencies and an example configuration file.
    git-svn-id: http://svn.rsence.org@574 5bb182ed-6ace-4cc9-85f1-9567b96542ab

---
+++ contrib/plugins/fsmount/fsmount.rb
@@ -0,0 +1,101 @@
+# -* coding: UTF-8 -*-
+###
+  # Riassence Core -- http://rsence.org/
+  #
+  # Copyright (C) 2009 Toni Nurminen <tjn@riassence.com>
+  #
+  # This file is part of Riassence Core.
+  #
+  # Riassence Core is free software: you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation, either version 3 of the License, or
+  # (at your option) any later version.
+  #
+  # Riassence Core is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+  #
+  # You should have received a copy of the GNU General Public License
+  # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  #
+  ###
+  
+require 'shared-mime-info'
+require 'yaml'
+class FSMount < ServletPlugin
+
+
+  def open
+    @conf = YAML::load(File.read(File.join(@path, '/conf/fileconfig')))
+  end
+
+  def uri_matches?(uri)
+    @conf.each_key do |uri_prefix|
+      return true if uri[0..(uri_prefix.length - 1)] == uri_prefix
+    end
+    return false
+  end
+  
+  def uri_to_path(uri)
+    @conf.each_key do |uri_prefix|
+      if uri[0..(uri_prefix.length - 1)] == uri_prefix
+        root_path = @conf[uri_prefix]['path']
+        rel_path = uri[uri_prefix.length..-1]
+        full_path = File.expand_path(File.join(root_path,rel_path))
+        return full_path
+      end
+    end
+  end
+  
+  def error_404(res)
+    res.status = 404
+    res['content-type'] = 'text/plain'
+    res.body = '404 - Not Found'
+  end
+  
+  def match( uri, request_type) #tsekkaa yamlista avaimet jotka ovat regexpejä
+    return (request_type == :get and uri_matches?(uri))
+  end
+  
+  def html_index(full_path, res)
+    res.status = 200
+    res['content-type'] = 'text/plain'
+    res.body = Dir.entries(full_path).join("\n")
+  end
+  
+  def serve_file(full_path, res)
+    begin
+      res.status = 200
+      res['content-type'] = MIME::check(full_path).type
+      content = File.read(full_path)
+      res['content-length'] = content.length
+      res.body = content
+    rescue
+      res.status = 503
+      res['content-type'] = 'text/plain'
+      res.body = '503 - Generic Server Error'
+    end
+  end
+  
+  def get( req, res, ses)
+    uri = req.fullpath
+    unless uri_matches?(uri) 
+      error_404(res)
+      return
+    end
+    full_path = uri_to_path(uri)
+    unless File.exist?(full_path)
+      error_404(res)
+      return
+    end
+    if File.directory?(full_path)
+      html_index(full_path, res)
+    else
+      serve_file(full_path, res)
+    end
+  end
+  
+  
+end
+FSMount.new
