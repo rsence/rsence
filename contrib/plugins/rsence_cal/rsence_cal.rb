cd7f52c71bdc67d158a45c84ee1b94ec0c7fdc1f
Juha-Jarmo Heinonen <o@rsence.org>
2009-11-05 05:48:40 +0000

    Implemented create/modify/delete entries
    git-svn-id: http://svn.rsence.org@678 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- contrib/plugins/rsence_cal/rsence_cal.rb
+++ contrib/plugins/rsence_cal/rsence_cal.rb
@@ -20,7 +20,7 @@
   # along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #
   ###
-
+require 'pp'
 class RiassenceCal < Plugin
   def init
     @cal_path = File.join( @path, 'db', 'rsence_cal.db' )
@@ -48,6 +48,12 @@ class RiassenceCal < Plugin
       :entries_tab   => 0,
       :entries_day_name => 'Saturday 31.10.',
       :entries_day   => [],
+      :entries_day_edit => {
+        'create'   => [],
+        'delete'   => [],
+        'modify'   => [],
+        'response' => []
+      },
       :entries_wday0_name => 'Mon 26.10.',
       :entries_wday0 => [],
       :entries_wday1_name => 'Tue 27.10.',
@@ -65,7 +71,6 @@ class RiassenceCal < Plugin
     }
     default_keep = [ :calendar_day, :calendar_id, :entries_tab ]
     default_values.each do |value_name, default_value|
-      puts "#{value_name.inspect} => #{default_value.inspect}"
       if not ses.has_key?( value_name )
         ses[value_name] = HValue.new( msg, default_value )
       elsif not default_keep.include?( value_name )
@@ -76,7 +81,8 @@ class RiassenceCal < Plugin
     update_entries( msg )
     values_bind = [
       [:calendar_day, 'change_calendar_day'],
-      [:calendar_id,  'change_calendar_id' ]
+      [:calendar_id,  'change_calendar_id' ],
+      [:entries_day_edit, 'edit_entry' ]
     ]
     values_bind.each do |value_name,method_name|
       ses[value_name].bind( 'rsence_cal', method_name )
@@ -92,6 +98,53 @@ class RiassenceCal < Plugin
     @gui.init( msg, params )
   end
   
+  def edit_entry( msg, hvalue )
+    data = hvalue.data.clone
+    pp data
+    ses = get_ses( msg )
+    cal_id = ses[:calendar_id].data
+    cal_day = ses[:calendar_day].data
+    data['response'] = []
+    data['create'].each do |item|
+      time_begin = cal_day + (item['timeBegin']*60*60)
+      time_end = cal_day + (item['timeEnd']*60*60)
+      item_id = @db[:cal_entries].insert(
+        :time_begin  => time_begin,
+        :time_end    => time_end,
+        :title       => item['label'],
+        :calendar_id => cal_id
+      )
+      puts "item_id: #{item_id.inspect}"
+      data['response'].push( {
+        'id' => item['id'],
+        'modify' => {
+          'id' => item_id
+        }
+      } )
+      pp data['response']
+    end
+    data['create'] = []
+    data['modify'].each do |item|
+      time_begin = cal_day + (item['timeBegin']*60*60)
+      time_end = cal_day + (item['timeEnd']*60*60)
+      @db[:cal_entries].filter(
+        :id => item['id']
+      ).update(
+        :time_begin => time_begin,
+        :time_end => time_end,
+        :title => item['label']
+      )
+    end
+    data['modify'] = []
+    data['delete'].each do |item_id|
+      @db[:cal_entries].filter(
+        :id => item_id
+      ).delete
+    end
+    data['delete'] = []
+    hvalue.set( msg, data )
+    return true
+  end
   def change_calendar_day( msg, hvalue )
     update_entries_day_names( msg )
     update_entries( msg )
@@ -199,13 +252,13 @@ private
   end
   def same_week_offset( time, target_wday )
     day_secs = 60*60*24
-    wday_sel = Time.at(time).wday - 1
+    wday_sel = Time.at(time).utc.wday - 1
     wday_sel = 6 if wday_sel == -1
     return (target_wday-wday_sel)*day_secs
   end
   def update_entries_day_names( msg )
     ses = get_ses( msg )
-    time = Time.at( ses[:calendar_day].data )
+    time = Time.at( ses[:calendar_day].data ).utc
     names = {
       :long  => %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday),
       :short => %w(Sun Mon Tue Wed Thu Fri Sat)
@@ -225,8 +278,8 @@ private
     end
   end
   def time_to_decimal_hour( time )
-    t = Time.at( time )
-    time_begin_day = Time.mktime( t.year, t.month, t.mday, 0, 0, 0 ).to_i
+    t = Time.at( time ).utc
+    time_begin_day = Time.gm( t.year, t.month, t.mday, 0, 0, 0 ).to_i
     secs = time - time_begin_day
     mins = secs / 60.0
     hour = ( (mins / 60.0) * 2 ).round * 0.5
@@ -237,16 +290,18 @@ private
     time_end = time_begin+86400
     @db[:cal_entries].filter(
       :calendar_id => cal_id,
-      :time_begin  => time_begin..time_end,
-      :time_end    => time_begin..time_end
+      :time_begin  => time_begin...time_end
     ).each do |entry_row|
+      row_begin = time_to_decimal_hour( entry_row[:time_begin].to_i )
+      row_end = time_to_decimal_hour( entry_row[:time_end  ].to_i )
+      if row_end < row_begin and row_end == 0
+        row_end = 24
+      end
       entries.push({
-        :id => entry_row[:id],
-        :label => entry_row[:title],
-        :value => [
-          time_to_decimal_hour( entry_row[:time_begin].to_i ),
-          time_to_decimal_hour( entry_row[:time_end  ].to_i )
-        ]
+        'id' => entry_row[:id],
+        'label' => entry_row[:title],
+        'timeBegin' => row_begin,
+        'timeEnd'   => row_end
       })
     end
     return entries
@@ -254,8 +309,8 @@ private
   def update_entries( msg )
     ses = get_ses( msg )
     cal_id = ses[:calendar_id].data
-    t = Time.at( ses[:calendar_day].data )
-    time = Time.mktime( t.year, t.month, t.mday, 0, 0, 0 ).to_i
+    t = Time.at( ses[:calendar_day].data ).utc
+    time = Time.gm( t.year, t.month, t.mday, 0, 0, 0 ).to_i
     [ [ :entries_day,   0 ],
       [ :entries_wday0, same_week_offset( time, 0 ) ],
       [ :entries_wday1, same_week_offset( time, 1 ) ],
