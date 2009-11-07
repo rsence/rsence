1544492f9c871747914b40ebb544a83877936c76
Juha-Jarmo Heinonen <o@rsence.org>
2009-11-07 02:03:55 +0000

    Documentation
    git-svn-id: http://svn.rsence.org@686 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/src/js/comm/jsloader/jsloader.js
+++ trunk/src/js/comm/jsloader/jsloader.js
@@ -6,9 +6,21 @@
  *   with this software package. If not, contact licensing@riassence.com
  */
 
-
+/*** = Description
+  ** A class for asynchronously fetching Javascript libraries from the server.
+  **
+  ** Loads and evalueates the code returned as a string from the server.
+  ** Use the jsLoader instance to get packaged Javascript libraries from the
+  ** standard package url.
+***/
 JSLoader = HClass.extend({
   
+/** = Description
+  * Construct with the base url.
+  *
+  * The this is the base url used by the +load+ method.
+  *
+  **/
   constructor: function(_uri){
     var _this = this;
     _this._loadedJS = [];
@@ -16,10 +28,26 @@ JSLoader = HClass.extend({
     _this._okayed = false;
   },
   
+  // Error catcher for failed requests.
   _fail: function(_this,_resp){
     console.log("failed to load js: "+_resp.url);
   },
   
+/** = Description
+  * Loads a js package using the name.
+  *
+  * The base url given in the constructor is used as the prefix.
+  * Omit the '.js' suffix, because it's appended automatically.
+  *
+  * = Parameters
+  * +_jsName+::   The name of the js file to load (without the .js suffix)
+  *
+  * = Usage:
+  * Uses the main instance set to the base path of the server's
+  * js package url. Loads a package containing list components.
+  *   jsLoader.load('lists');
+  *
+  **/
   load: function(_jsName){
     var _this = this;
     if((_this._loadedJS.indexOf(_jsName)!==-1)) {
@@ -42,6 +70,8 @@ JSLoader = HClass.extend({
   
 });
 
+// Makes the standard jsLoader instance based on the client base url 
+// of the server when the page is loaded.
 RUN("jsLoader = JSLoader.nu(HCLIENT_BASE+'/js/');");
 
 
