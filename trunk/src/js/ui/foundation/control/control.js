fce967cafb1fb831414d8dea4a0c61cc3e00854d
Toni Nurminen <toni.nurminen@rsence.org>
2009-11-08 14:27:29 +0000

    Documentation update
    git-svn-id: http://svn.rsence.org@703 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/src/js/ui/foundation/control/control.js
+++ trunk/src/js/ui/foundation/control/control.js
@@ -6,44 +6,41 @@
  *   with this software package. If not, contact licensing@riassence.com
  */
 
-
-/** class: HControl
+/** = Description
+  * Abstract foundation class for all active visual components that 
+  * implement events and values.
+  * 
+  * Feel free to extend HControl to suit your needs. See any component 
+  * for extension reference.
   *
-  * Abstract foundation class for all active visual components that implement events and values.
-  *
-  * Feel free to extend HControl to suit your needs. See any component for extension reference.
-  *
-  * vars: Instance variables (common for almost all components)
-  *  type - '[HControl]'
-  *  label - The visual value of a component. See <setLabel>.
-  *  action - A function reference to call in certain situations.
-  *  events - A structure that tells what events to bind.
-  *  enabled - The enabled/disabled flag. See <setEnabled>.
-  *  value - The current value of a component. See <setValue>.
-  *  valueObj - The current <HValue>-compatible object. Do not set directly. Holds reference to the bound <HValue> instance. Set with <HValue.bind>.
-  *  minValue - The minimum allowed value, when the component utilizes value ranges. See <setValueRange>.
-  *  maxValue - The maximum allowed value, when the component utilizes value ranges. See <setValueRange>.
-  *  active - A boolean value that shows whether this control is currently active or not. Control gets active when the user clicks on it.
-  *
-  * Extends:
-  *  <HView>
-  *
-  * See Also:
-  *  <HSystem> <HApplication> <HView> <HValue> <HEventManager>
-  *
+  * = Instance variables
+  * +type+::        '[HControl]'
+  * +label+::       The visual value of a component. See setLabel.
+  * +action+::      A function reference to call in certain situations.
+  * +events+::      A structure that tells what events to bind.
+  * +enabled+::     The enabled/disabled flag. See setEnabled.
+  * +value+::       The current value of a component. See setValue.
+  * +valueObj+::    The current HValue compatible object. Do not set directly.
+  *                 Holds reference to the bound HValue instance.
+  *                 Set with HValue.bind.
+  * +minValue+::    The minimum allowed value, when the component
+  *                 utilizes value ranges. See <setValueRange>.
+  * +maxValue+::    The maximum allowed value, when the component
+  *                 utilizes value ranges. See <setValueRange>.
+  * +active+::      A boolean value that shows whether this control is currently 
+  *                 active or not. Control gets active when the user clicks on it.
   **/
 HControl = HView.extend({
   
   componentBehaviour: ['view','control'],
-/** constructor: constructor
-  *
+/** = Description
   * The first two parameters are the same as with <HView>, but additionally
   * sets the label and events.
   *
-  * Parameters:
-  *  _rect - The rectangle of the component. See <HView.constructor>.
-  *  _parent - The parent component of the component. See <HView.constructor>.
-  *  _options - (optional) All other parameters. See <HComponentDefaults>.
+  * = Parameters
+  *  +_rect+::   The rectangle of the component. See <HView.constructor>.
+  *  +_parent+:: The parent component of the component. See <HView.constructor>.
+  *  +options+:: optional, all other parameters. See <HComponentDefaults>.
   *
   **/
   refreshOnValueChange: true,
@@ -103,16 +100,19 @@ HControl = HView.extend({
     }
   },
   
-/** method: setAction
+/** = Description
+  * Actions are specified as function calls to components. 
+  * These are usually used as defineable external function calls.
   *
-  * Actions are specified as function calls to components. These are usually used as defineable external function calls.
+  * = Parameters
+  * +_action+::A function reference, the number of parameters the receiver function should take depends on the component.
   *
-  * Parameters:
-  *  _action - A function reference, the number of parameters the receiver function should take depends on the component.
+  * = Returns
+  * +self+
   *
-  * Examples:
-  *  > myComponent.setAction(alert);
-  *  > myComponent.setAction(function(param){window.status='param given: '+param});
+  * = Usage
+  *   myComponent.setAction(alert);
+  *   myComponent.setAction(function(param){window.status='param given: '+param});
   *
   **/
   setAction: function(_action) {
@@ -120,12 +120,7 @@ HControl = HView.extend({
     return this;
   },
   
-/** method: die
-  *
-  * Unregisters events before destroying the view.
-  *
-  * See also:
-  *  <HView.die>
+/** Unregisters events before destroying the view.
   **/
   die: function() {
     var _this = this;
@@ -137,14 +132,16 @@ HControl = HView.extend({
     _this.base();
   },
   
-/** method: setLabel
-  *
+/** = Description
   * Sets the label on a control component: the text that's displayed, for
   * example, in the <HButton>. Actual functionality is implemented in component
   * templates and component <refresh> method extensions.
   *
-  * Parameters:
-  *  _label - The text the component should display.
+  * = Parameters
+  *  +_label+::  The text the component should display.
+  *
+  * = Returns
+  * +self+
   *
   **/
   setLabel: function(_label) {
@@ -158,18 +155,17 @@ HControl = HView.extend({
     return this;
   },
   
-/** method: setEnabled
-  *
+/** = Description
   * Enables the Control if the enabled flag is true, and disables
   * it if enabled is false.
   * Typically, a disabled Control also won't post messages or respond
   * to mouse and keyboard manipulation.
   *
-  * Parameters:
-  *  _flag - Boolean; true enables, false disables.
+  * = Parameters
+  *  +_flag+::  Boolean; true enables, false disables.
   *
-  * See also:
-  *  <events> <HEventManager.register> <HEventManager.unreg>
+  * = Returns
+  * +this+
   *
   **/
   setEnabled: function(_flag) {
@@ -207,18 +203,19 @@ HControl = HView.extend({
   },
   
   
-/** method: setValueRange
-  *
+/** = Description
   * Assigns the object a new value range. Used for sliders etc. Calls 
-  * <setValue> with the value given.
+  * setValue with the value given.
   *
-  * Parameters:
-  *  _value - The new <value> to be set to the component's <HValue>-compatible instance.
-  *  _minValue - The new minimum <value> limit. See <minValue>.
-  *  _maxValue - The new maximum <value> limit. See <maxValue>.
+  * = Parameters
+  *  +_value+::     The new <value> to be set to the component's 
+  *                 HValue compatible instance.
+  *  +_minValue+::  The new minimum <value> limit. See <minValue>.
+  *  +_maxValue+::  The new maximum <value> limit. See <maxValue>.
   *
-  * See also:
-  *  <setValue> <HValue> <minValue> <maxValue> <HValueManager> <refresh>
+  * = Returns
+  * +self+
+  *
   **/
   setValueRange: function(_value, _minValue, _maxValue) {
     this.minValue = _minValue;
@@ -230,6 +227,8 @@ HControl = HView.extend({
     return this;
   },
   
+/** Refreshes the value of HControl.
+  **/
   refreshValue: function(){
     if(this.markupElemIds){
       if(this.markupElemIds.value){
@@ -238,6 +237,9 @@ HControl = HView.extend({
     }
     return this;
   },
+  
+  /** Refreshes the HControl's label.
+    **/
   refreshLabel: function(){
     if(this.markupElemIds){
       if(this.markupElemIds.label){
@@ -246,6 +248,8 @@ HControl = HView.extend({
     }
     return this;
   },
+  /** Refreshes the value and label if they are not up to date.
+    **/
   refresh: function(){
     this.base();
     if(this.drawn){
@@ -262,7 +266,8 @@ HControl = HView.extend({
   
   
   // Class methods and properties
-  
+/** Stops propagation
+  **/
   stopPropagation: function(event) {
     if (event.stopPropagation) { 
       event.stopPropagation(); 
