d6f7bd652ff6e8f1cbe4801166cb62ac9976c594
Toni Nurminen <toni.nurminen@rsence.org>
2009-11-08 19:31:48 +0000

    Documentation update
    git-svn-id: http://svn.rsence.org@710 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/src/js/ui/foundation/control/valueresponder/valueresponder.js
+++ trunk/src/js/ui/foundation/control/valueresponder/valueresponder.js
@@ -6,18 +6,22 @@
  *   with this software package. If not, contact licensing@riassence.com
  */
 
+/*** = Description
+  **  Value responder class.
+  ***/
+
 HValueResponder = HClass.extend({
   
-/** method: setValueObj
+/** = Description
+  * Binds an HValue compatible instance to the component's valueObj. Also 
+  * calls setValue. It should not be called from user code, instead use HValue.bind.
   *
-  * Binds an <HValue>-compatible instance to the component's valueObj. Also 
-  * calls <setValue>. It should not be called from user code, instead use <HValue.bind>.
+  * = Parameter
+  *  +_aValueObj+::  The new value object.
   *
-  * Parameter:
-  *  _aValueObj - The new value object.
+  * = Returns
+  *  +self+
   *
-  * See also:
-  *  <setValue> <setValueRange> <HValue.bind> <HValue.unbind> <HValueManager>
   **/
   setValueObj: function(_valueObj) {
     this.valueObj = _valueObj;
@@ -25,22 +29,27 @@ HValueResponder = HClass.extend({
     return this;
   },
   
+/** = Description
+  * Checks if the value given as parameter differs from this.value.
+  *
+  * = Parameters
+  * +_value+::  The value to be tested.
+  *
+  **/
   valueDiffers: function(_value){
     return (COMM.Values.encode(_value) !== COMM.Values.encode(this.value));
   },
-
   
-/** method: setValue
-  *
+/** = Description
   * Assigns the object a new value. Extend it, if your component needs to do
   * something whenever the value changes.
   *
-  * Parameter:
-  *  _value - The new value. Allowed values depend on the component type 
-  *           and other usage of the bound <HValue> instance.
+  * = Parameter
+  *  +_value+::   The new value. Allowed values depend on the component type
+  *               and other usage of the bound HValue instance.
   *
-  * See also:
-  *  <setValueRange> <HValue> <HValueManager> <refresh>
+  * = Returns
+  * +self+
   *
   **/
   setValue: function(_value) {
