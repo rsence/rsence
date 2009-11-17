67fa477de9acb3ca0ce6a9910747c182581af3db
Toni Nurminen <toni.nurminen@rsence.org>
2009-11-17 23:34:49 +0000

    Documentation update
    git-svn-id: http://svn.rsence.org@776 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/experimental/src/js/ui/controls/radiobutton/radiobutton.js
+++ branches/experimental/src/js/ui/controls/radiobutton/radiobutton.js
@@ -6,36 +6,29 @@
  *   with this software package. If not, contact licensing@riassence.com
  */
 
-/** class: HRadioButton
-  *
-  * A HRadioButton is a labeled, two-state button that's displayed in a group along
-  * with other similar buttons. The button itself is a round icon that has a filled
-  * center when the HRadioButton is turned on, and is empty when it's off.
-  * The label appears next to the icon.
-  *
-  * Only one radio button in the group can be on at a time; when the user clicks a
-  * button to turn it on, the button that's currently on is turned off. One button
-  * in the group must be on at all times; the user can turn a button off only by
-  * turning another one on. The button that's on has a value of 1 (B_CONTROL_ON);
-  * the others have a value of 0 (B_CONTROL_OFF).
-  *
-  * The BRadioButton class handles the interaction between radio buttons in the
-  * following way: A direct user action can only turn on a radio button, not turn
-  * it off. However, when the user turns a button on, the BRadioButton object turns
-  * off all sibling BRadioButtons—that is, all BRadioButtons that have the same
-  * parent as the one that was turned on.
-  *
-  * This means that a parent view should have no more than one group of radio buttons
-  * among its children. Each set of radio buttons should be assigned a separate
-  * parent—perhaps an empty BView that simply contains the radio buttons and does
-  * no drawing of its own.
-  *
-  * Extends:
-  *  <HCheckbox>
-  *
-  * See also:
-  *  <HControl> <HCheckbox> <HToggleButton>
-  **/
+/*** = Description
+  ** A HRadioButton is a labeled, two-state button that's displayed in a group along
+  ** with other similar buttons. The button itself is a round icon that has a filled
+  ** center when the HRadioButton is turned on, and is empty when it's off.
+  ** The label appears next to the icon.
+  **
+  ** Only one radio button in the group can be on at a time; when the user clicks a
+  ** button to turn it on, the button that's currently on is turned off. One button
+  ** in the group must be on at all times; the user can turn a button off only by
+  ** turning another one on. The button that's on has a value of 1 (B_CONTROL_ON);
+  ** the others have a value of 0 (B_CONTROL_OFF).
+  **
+  ** The BRadioButton class handles the interaction between radio buttons in the
+  ** following way: A direct user action can only turn on a radio button, not turn
+  ** it off. However, when the user turns a button on, the BRadioButton object turns
+  ** off all sibling BRadioButtons—that is, all BRadioButtons that have the same
+  ** parent as the one that was turned on.
+  **
+  ** This means that a parent view should have no more than one 
+  ** group of radio buttons among its children. Each set of radio buttons 
+  ** should be assigned a separate parent—perhaps an empty BView that 
+  ** simply contains the radio buttons and does no drawing of its own.
+  ***/
 
 HRadioButton = HCheckbox.extend(
   HValueMatrixComponentExtension
