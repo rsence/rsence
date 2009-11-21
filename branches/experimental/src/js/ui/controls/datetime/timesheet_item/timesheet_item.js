273ff676ff7f7f0ac2531864d84ac6ef35b198da
Toni Nurminen <toni.nurminen@rsence.org>
2009-11-21 01:15:02 +0000

    Documentation update
    git-svn-id: http://svn.rsence.org@797 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/experimental/src/js/ui/controls/datetime/timesheet_item/timesheet_item.js
+++ branches/experimental/src/js/ui/controls/datetime/timesheet_item/timesheet_item.js
@@ -6,11 +6,23 @@
  *   with this software package. If not, contact licensing@riassence.com
  */
 
+/*** = Description
+  ** Item class to be used with HTimeSheet.
+  ***/
 HTimeSheetItem = HControl.extend({
   componentName: 'timesheet_item',
   dragMode: 'create',
   prevXY: [0,0],
   prevXYTime: 0,
+  
+/** = Description
+  * Dragging is used to change coordinates.
+  *
+  * = Parameters
+  * +x+:: X coordinate at the start of drag.
+  * +y+:: Y coordinate at the start of drag.
+  *
+  **/
   startDrag: function(x,y){
     this.origY = y-this.parent.pageY();
     if(this.dragMode === 'normal'){
@@ -46,10 +58,27 @@ HTimeSheetItem = HControl.extend({
     this.prevXYTime = _timeNow;
     return true;
   },
+  
+/** = Description
+  * Label setter function.
+  *
+  * = Parameters
+  * +_label+:: New label
+  *
+  **/
   setTimeSheetItemLabel: function(_label){
     this.label = _label;
     this.refreshLabel();
   },
+  
+/** = Description
+  * Function used to calculate the right size for a new 
+  * item created by dragging.
+  *
+  * = Parameters
+  * +_y+:: Y coordinate at the start of drag.
+  *
+  **/
   dragCreate: function(_y){
     var _negative = (_y < this.origY),
         _lineHeight = Math.floor(this.parent.pxPerHour/2),
@@ -85,6 +114,13 @@ HTimeSheetItem = HControl.extend({
     this.rect.setTop(_top);
     this.rect.setBottom(_bottom);
   },
+  
+/** = Description
+  * Resize top by dragging auxiliary function.
+  *
+  * = Parameters
+  * +_y+:: Y coordinate at the start of drag.
+  **/
   dragResizeTop: function(_y){
     var _lineHeight = Math.floor(this.parent.pxPerHour/2),
         _top = Math.floor( _y/_lineHeight )*_lineHeight;
@@ -94,6 +130,14 @@ HTimeSheetItem = HControl.extend({
     }
     this.rect.setTop( _top );
   },
+  
+/** = Description
+  * Resize function for resizing the bottom of item.
+  *
+  * = Parameters
+  * +_y+:: Y coordinate at the start of drag.
+  *
+  **/
   dragResizeBottom: function(_y){
     var _lineHeight = Math.floor(this.parent.pxPerHour/2),
         _bottom = Math.floor( _y/_lineHeight )*_lineHeight;
@@ -103,6 +147,14 @@ HTimeSheetItem = HControl.extend({
     }
     this.rect.setBottom( _bottom );
   },
+  
+/** = Description
+  * Move function for item by dragging and dropping.
+  *
+  * = Parameters
+  * +_y+:: Y coordinate at the start of drag.
+  *
+  **/
   dragMove: function(_y){
     var _lineHeight = Math.floor(this.parent.pxPerHour/2),
         _top = Math.floor( (0-this.moveDiff+_y)/_lineHeight )*_lineHeight;
@@ -112,6 +164,16 @@ HTimeSheetItem = HControl.extend({
     }
     this.rect.offsetTo( this.rect.left, _top );
   },
+  
+/** = Description
+  * Drag function for item. Decides whether the user wants to create a new
+  * item, resize top, resize bottom or move an existing item.
+  *
+  * = Parameters
+  * +x+:: X coordinate at the start of drag.
+  * +y+:: Y coordinate at the start of drag.
+  *
+  **/
   doDrag: function(x,y){
     var _pageY  = this.parent.pageY(),
         _y = y - _pageY;
@@ -130,6 +192,15 @@ HTimeSheetItem = HControl.extend({
     this.drawRect();
     return true;
   },
+  
+/** = Description
+  * Modifies the existing item's coordinates or creates a new one.
+  *
+  * = Parameters
+  * +x+:: X coordinate at the end of drag.
+  * +y+:: Y coordinate at the end of drag.
+  *
+  **/
   endDrag: function(x,y){
     var _pxPerHour = Math.floor(this.parent.pxPerHour),
         _value;
@@ -156,6 +227,11 @@ HTimeSheetItem = HControl.extend({
     this.dragMode = 'normal';
     return true;
   },
+  
+/** = Description
+  * Refreshes the object's label and place on the HTimeSheet.
+  *
+  **/
   refreshValue: function(){
     var _value = this.value,
         _pxPerHour = this.parent.pxPerHour;
