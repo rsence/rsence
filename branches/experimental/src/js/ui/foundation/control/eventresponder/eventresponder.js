2f31555813aec093e67d2bcb56cc2420d36b1781
Toni Nurminen <toni.nurminen@rsence.org>
2009-11-25 20:56:09 +0000

    Documentation update
    git-svn-id: http://svn.rsence.org@834 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/experimental/src/js/ui/foundation/control/eventresponder/eventresponder.js
+++ branches/experimental/src/js/ui/foundation/control/eventresponder/eventresponder.js
@@ -14,56 +14,38 @@
   ** Pre-defined event handler methods, extend these in your subclass.
   **
   ** +focus+::               Called when the component gets focus
-  **
   ** +blur+::                Called when the component loses focus
-  **
   ** +mouseDown+::           Called when the mouse button is pushed down
-  **
   ** +mouseUp+::             Called when the mouse button is released
-  **
   ** +mouseWheel+::          Called when the mouse wheel is used
-  **
   ** +startDrag+::           Called when the mouse button 
   **                         is pressed (and item is draggable).
-  **
   ** +endDrag+::             Called when the mouse button 
   **                         is released (and item is draggable).
-  **
   ** +doDrag+::              Called when the mouse is moved and mouse button 
   **                         is down (and item is draggable).
-  **
   ** +onDrop+::              Called when a draggable item is released 
   **                         on the droppable.
-  **
   ** +onHoverStart+::        Called when a draggable item is moved 
   **                         over the droppable.
-  **
   ** +onHoverEnd+::          Called when a draggable item is moved out 
   **                         of the droppable.
-  **
   ** +keyDown+::             Called when the user presses a key, and 
   **                         the control is active.
-  **
   ** +keyUp+::               Called when the user releases a key, and 
   **                         the control is active.
-  **
   ** +textEnter+::           Called when the user releases a key regardless 
   **                         if the control is active or not.
-  **
   ** +gainedActiveStatus+::  Called when the component gets activated.
-  **
   ** +lostActiveStatus+::    Called when the component gets deactivated.
-  **
 ***/
 HEventResponder = HClass.extend({
   
 /** = Description
   * The event responder interface for +HControl+.
-  *
   * Registers the events defined by boolean properties of 
   * the events object to the control instance. The event manager 
   * handles the event mapping and abstraction itself.
-  *
   * NOTE startDrag vs mouseDown and endDrag vs mouseUp events 
   * conflict, if both are set simultaneously.
   *
@@ -73,25 +55,18 @@ HEventResponder = HClass.extend({
   *
   * = Event types
   * +mouseMove+::   flag, start listening to global mousemove events.
-  *
   * +mouseDown+::   flag, start listening to mouseDown events when the 
   *                 component has focus.
-  *
   * +mouseUp+::     flag, start listening to mouseUp events when the 
   *                 component has focus.
-  *
   * +mouseWheel+::  flag, start listening to mouseWheel events when the 
   *                 component has focus.
-  *
   * +draggable+::   flag, start listening to drag events when the 
   *                 component has focus.
-  *
   * +droppable+::   flag, start listening to drop events when the 
   *                 component has focus.
-  *
   * +keyDown+::     flag, start listening to keyDown events when the 
   *                 component has focus.
-  *
   * +keyUp+::       flag, start listening to keyUp events when the 
   *                 component has focus.
   *
@@ -166,8 +141,8 @@ HEventResponder = HClass.extend({
   * starts listening to click events when the component has focus.
   *
   * = Parameters
-  * +_flag+::  Set the click event listening on/off (true/false) for
-  *            the component instance.
+  * +_flag+:: Set the click event listening on/off (true/false) for
+  *           the component instance.
   *
   * = Returns
   * +self+
@@ -220,8 +195,8 @@ HEventResponder = HClass.extend({
   * starts listening to mouseWheel events when the component has focus.
   *
   * = Parameters
-  * +_flag+::  Set the mouseWheel event listening on/off (true/false) for
-  *            the component instance.
+  * +_flag+:: Set the mouseWheel event listening on/off (true/false) for
+  *           the component instance.
   *
   * = Returns
   * +self+
@@ -239,7 +214,7 @@ HEventResponder = HClass.extend({
   *
   * = Parameters
   * +_flag+:: Set the startDrag, doDrag and endDrag event listening 
-  *            on/off (true/false) for the component instance.
+  *           on/off (true/false) for the component instance.
   *
   * = Returns
   * +self+
@@ -256,8 +231,8 @@ HEventResponder = HClass.extend({
   * onHoverEnd -events depending on the value of the flag argument.
   *
   * = Parameters
-  * +_flag+::  Set the onHoverStart, onDrop and onHoverEnd event listening 
-  *            on/off (true/false) for the component instance.
+  * +_flag+:: Set the onHoverStart, onDrop and onHoverEnd event listening 
+  *           on/off (true/false) for the component instance.
   *
   * = Returns
   * +self+
@@ -294,8 +269,8 @@ HEventResponder = HClass.extend({
   * the value of the flag argument.
   *
   * = Parameters
-  * +_flag+::  Set the keyUp event listening on/off (true/false) for
-  *            the component instance.
+  * +_flag+:: Set the keyUp event listening on/off (true/false) for
+  *           the component instance.
   *
   * = Returns
   * +self+
@@ -329,7 +304,6 @@ HEventResponder = HClass.extend({
   
 /** = Description
   * Default focus event responder method. Does nothing by default.
-  *
   * Called when the component gets focus.
   *
   **/
@@ -337,7 +311,6 @@ HEventResponder = HClass.extend({
   
 /** = Description
   * Default blur event responder method. Does nothing by default.
-  *
   * Called when the component loses focus.
   *
   **/
@@ -346,13 +319,12 @@ HEventResponder = HClass.extend({
   
 /** = Description
   * Default gainedActiveStatus event responder method. Does nothing by default.
-  *
   * Called when the component gains active status; both focused and clicked.
   *
   * = Parameters
-  * +_lastActiveControl+::  A reference to the control that was active
-  *                         before this control became active. Can
-  *                         be null if there was no active control.
+  * +_lastActiveControl+:: A reference to the control that was active
+  *                        before this control became active. Can
+  *                        be null if there was no active control.
   *
   **/
   gainedActiveStatus: function(_lastActiveControl) {
@@ -376,7 +348,6 @@ HEventResponder = HClass.extend({
   
 /** = Description
   * Default lostActiveStatus event responder method. Does nothing by default.
-  *
   * Called when the component loses active status; another component was
   * focused and clicked.
   *
@@ -390,7 +361,7 @@ HEventResponder = HClass.extend({
     
   },
   
-  // A low-level handler for lost active status, don't extend this.
+  // --A low-level handler for lost active status, don't extend this.++
   _lostActiveStatus: function(_newActiveControl) {
     if(this.enabled) {
       this.toggleCSSClass(this.elemId, HControl.CSS_ACTIVE, false);
@@ -401,15 +372,12 @@ HEventResponder = HClass.extend({
   
 /** = Description
   * Default mouseMove event responder method. Does nothing by default.
-  *
   * Called whenever the mouse cursor is moved regardless if the
   * component is active or has focus.
   *
   * = Parameters
-  * +x+::           The horizonal coordinate units (px) of the 
-  *                 mouse cursor position.
-  * +y+::           The vertical coordinate units (px) of the 
-  *                 mouse cursor position.
+  * +x+:: The horizonal coordinate units (px) of the mouse cursor position.
+  * +y+:: The vertical coordinate units (px) of the mouse cursor position.
   *
   **/
   mouseMove: function(x,y) {},
@@ -462,21 +430,20 @@ HEventResponder = HClass.extend({
   * Default mouseWheel event responder method. Does nothing by default.
   *
   * = Parameters
-  * +_delta+::  Scrolling delta, the wheel angle change. If delta is positive,
-  *             wheel was scrolled up. Otherwise, it was scrolled down.
+  * +_delta+:: Scrolling delta, the wheel angle change. If delta is positive,
+  *            wheel was scrolled up. Otherwise, it was scrolled down.
   *
   **/
   mouseWheel: function(_delta) {},
   
 /** = Description
   * Default startDrag event responder method. Sets internal flags by default.
-  *
   * This is the preferred method to extend if you want to do something
   * when a drag event starts. If you extend, remember to call +this.base();+
   *
   * = Parameters
-  * +x+::    The horizonal coordinate units (px) of the mouse cursor position.
-  * +y+::    The vertical coordinate units (px) of the mouse cursor position.
+  * +x+:: The horizonal coordinate units (px) of the mouse cursor position.
+  * +y+:: The vertical coordinate units (px) of the mouse cursor position.
   *
   **/
   startDrag: function(x, y) {
@@ -485,7 +452,6 @@ HEventResponder = HClass.extend({
   
 /** = Description
   * Default doDrag event responder method. Does nothing by default.
-  *
   * This is the preferred method to extend while a drag method is ongoing.
   * Called whenever the mouse cursor moves and a drag event has been started.
   *
@@ -498,13 +464,12 @@ HEventResponder = HClass.extend({
   
 /** = Description
   * Default startDrag event responder method. Sets internal flags by default.
-  *
   * This is the preferred method to extend if you want to do something
   * when a drag event ends. If you extend, remember to call +this.base();+
   *
   * = Parameters
-  * +x+::  The horizonal coordinate units (px) of the mouse cursor position.
-  * +y+::  The vertical coordinate units (px) of the mouse cursor position.
+  * +x+:: The horizonal coordinate units (px) of the mouse cursor position.
+  * +y+:: The vertical coordinate units (px) of the mouse cursor position.
   *
   **/
   endDrag: function(x, y) {
@@ -514,79 +479,72 @@ HEventResponder = HClass.extend({
 
 /** = Description
   * Default onDrop event responder method. Does nothing by default.
-  *
   * Extend the onDrop method, if you want to do something 
   * when this instance is the target of another instance's endDrag event.
   * Called when a dragged component instance is dropped on the target instance.
   *
   * = Parameters
-  * +obj+::  The dragged component object.
+  * +obj+:: The dragged component object.
   *
   **/
   onDrop: function(obj) {},
 
 /** = Description
   * Default onHoverStart event responder method. Does nothing by default.
-  *
   * Extend the onDrop method, if you want to do something 
   * when this instance is the target of another instance's doDrag event.
   * Called when a dragged component instance is dragged over
   * the target instance.
   *
   * = Parameters
-  * +obj+::  The dragged component object.
+  * +obj+:: The dragged component object.
   *
   **/
   onHoverStart: function(obj) {},
   
 /** = Description
   * Default onHoverStart event responder method. Does nothing by default.
-  *
   * Extend the onDrop method, if you want to do something 
   * when this instance is no longer the target of another instance's
-  * doDrag event.
-  * Called when a dragged component instance is dragged away from
+  * doDrag event. Called when a dragged component instance is dragged away from
   * the target instance.
   *
   * = Parameters
-  * +obj+::  The dragged component object.
+  * +obj+:: The dragged component object.
   *
   **/
   onHoverEnd: function(obj) {},
   
 /** = Description
   * Default keyDown event responder method. Does nothing by default.
-  *
   * Extend the keyDown method, if you want to do something
   * when a key is pressed and the component is active.
   *
   * = Parameters
-  * +_keycode+::  The keycode of the key that was pressed.
+  * +_keycode+:: The keycode of the key that was pressed.
   *
   **/
   keyDown: function(_keycode) {},
   
 /** = Description
   * Default keyUp event responder method. Does nothing by default.
-  *
   * Extend the keyUp method, if you want to do something
   * when a key is released and the component is active.
   *
   * = Parameters
-  * +_keycode+::  The keycode of the key that was released.
+  * +_keycode+:: The keycode of the key that was released.
   *
   **/
   keyUp: function(_keycode) {},
   
 /** = Description
   * Default textEnter event responder method. Does nothing by default.
-  *
   * Extend the textEnter method, if you want to do something
   * when a key is released regardless if the component is active,
   * has focus or not.
   *
   * = Parameters
-  * +_keycode+::  The keycode of the key that was released.
+  * +_keycode+:: The keycode of the key that was released.
   *
   **/
   textEnter: function() {},
