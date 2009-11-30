33d127164ee98666d3a52789b6d3f6984fae0825
Juha-Jarmo Heinonen <o@rsence.org>
2009-11-30 22:35:30 +0000

    Fixed and simplified HProgressBar
    git-svn-id: http://svn.rsence.org@858 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- branches/experimental/src/js/ui/controls/progress/progressbar/progressbar.js
+++ branches/experimental/src/js/ui/controls/progress/progressbar/progressbar.js
@@ -11,123 +11,23 @@
   ** such as a download or file transfer. In other words, it is a component 
   ** indicating a percentage of a total task has completed.
   **
-  ** = Instance variables
-  ** +value+::    Numeric value currently set to this object.
-  ** +minValue+:: The minimum value that can be set to this object.
-  ** +maxValue+:: The maximum value that can be set to this object.
-  ***/
-
+  ** Use the maxValue to define the point of progress at the end and use value
+  ** to define the point of progress.
+  **
+***/
 HProgressBar = HControl.extend({
-  
   componentName: "progressbar",
-  constructor: function(_rect,_parentClass,_options) {  
-
-    if(this.isinherited) {
-      this.base(_rect,_parentClass,_options);
-    }
-    else {
-      this.isinherited = true;
-      this.base(_rect,_parentClass,_options);
-      this.isinherited = false;
-    }
-    
-    if (!_options) {
-      _options = {};
-    }
-    
-    // Default options.
-    var _defaults = Base.extend({
-      value: 0,
-      minValue: 0,
-      maxValue: 100
-    });
-    
-    _defaults = _defaults.extend(_options);
-    _options = new _defaults();
-
-    this.value = _options.value;
-    this.minValue = _options.minValue;
-    this.maxValue = _options.maxValue;
-
-    this.visibleWidth = this.rect.width - 2;
-    this._progressbarPrefix = 'progressmark';
-    
-    if(!this.isinherited) {
-      this.draw();
-    }
-    
-    this.progressFrameHeight = 20;
-    this.progressFrames      = 10;
-    this.currProgressFrame   = 0;
-  },
-  /** Set frame height for progressbar
-    **/
-  setProgressFrameHeight: function(_px){
-    this.progressFrameHeight = _px;
-  },
-  /** Set frame numbers for progressbar
-    **/
-  setProgressFrameNum: function(_num){
-    this.progressFrames = _num;
-  },
   
-/** = Description
-  * Sets the current value of the object and extends the progress mark to the correct position.
-  * 
-  * = Parameters
-  * +_value+:: A numeric value to be set to the object.
-  *
-  **/  
-  setValue: function(_value) {  
-    this.base(_value);       
-    this.drawProgress();
-  },
+/** The amount of pixels the theme insets the width of the progress bar **/
+  themeWidthInset: 2,
   
-  /** = Description
-    * Sets the next frame on progressbar and draws it.
-    *
-    **/
-  onIdle: function(){
-    if(this.progressbarElemId) {
-      this.currProgressFrame++;
-      if(this.currProgressFrame>=this.progressFrames){this.currProgressFrame=0;}
-      var _px = this.currProgressFrame*this.progressFrameHeight;
-      ELEM.setStyle(this.progressbarElemId,'background-position','0px -'+_px+'px');
-    }
-  },
-  
-  /** Draws the rectangle and the markup of this object on the screen.
-    **/  
-  draw: function() {
-    if (!this.drawn) {
-      this.drawRect();
-      this.drawMarkup();
-      this._initProgress();
-    }
-  },
-
-// private method  
-  _initProgress: function() {
-    this.progressbarElemId = this.bindDomElement(
-      this._progressbarPrefix + this.elemId);
-    this.drawProgress();
-  },
-
-// private method  
-  _value2px: function() {   
-    var _intvalue = this.visibleWidth * ((this.value - this.minValue) / (this.maxValue - this.minValue));
-    var _pxvalue = parseInt(Math.round(_intvalue),10) + 'px';
-    return _pxvalue; 
-  },
-
-/** = Description
-  * Draws the progress.
-  *
-  **/
-  drawProgress: function() {
-    if (this.progressbarElemId) {
-      var _propval   = this._value2px();
-      ELEM.setStyle(this.progressbarElemId, 'width', _propval);
+/** Sets the width of the progress bar when the value changes. **/
+  refreshValue: function(){
+    if( this.drawn && this.markupElemIds.value ){
+      var _visibleWidth = ELEM.getVisibleSize( this.elemId )[0]-this.themeWidthInset,
+          _unitWidth = _visibleWidth/this.maxValue,
+          _progressWidth = Math.round(_unitWidth * this.value);
+      this.setStyleOfPart('value','width',_progressWidth+'px');
     }
   }
 });
