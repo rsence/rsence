f84b3b4552977c49adc499b95246199159bd50a3
Toni Nurminen <toni.nurminen@rsence.org>
2010-01-19 16:26:29 +0000

    Server side API documentation update
    git-svn-id: http://svn.rsence.org@903 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/lib/session/msg.rb
+++ trunk/lib/session/msg.rb
@@ -8,92 +8,76 @@
 
 module Riassence
 module Server
-
-=begin
-
-The Message object is initialized as 'msg' in SessionManager.
-It's passed around the system as the user/session -object namespace,
-much like 'self' is passed around in python methods.
-
-Using the msg object saves considerate amounts of cpu cycles and memory,
-because it allows single instances of any classes that handle user data.
-
-Major clean-up in 2008-05-07.
-
-=end
-
 require 'util/gzstring'
 
+
+## The +Message+ instance that contains session and request-response related 
+## mappings and utility methods.
+##
+## The Message object is initialized as 'msg' in SessionManager.
+## It's passed around the system as the user/session -object namespace,
+## much like 'self' is passed around in python methods.
+## 
+## Using the msg object saves considerate amounts of cpu cycles and memory,
+## because it allows single instances of any classes that handle user data.
 class Message
-  
-  attr_accessor :session, :ses_id,
-                :new_session, :restored_session,
-                :cloned_source, :cloned_targets,
-                :ses_valid, :request, :response,
-                :buffer, :response_success
-  
+  # Session data placeholder, assigned by SessionManager.
+  attr_accessor :session 
+  # The session identifier placeholder, assigned by SessionManager.
+  attr_accessor :ses_id
+  # New session flag, check it in your code to decide
+  # what to do, when a new session is encountered.
+  # In plugins, this usually means that some Values
+  # need to be created and bound or possibly that a
+  # user_id mapping needs to be done.
+  attr_accessor :new_session
+  # Old session first xhr flag, check it in your code
+  # to decide what to do, when a restored session is
+  # encountered. The old Values are automatically present,
+  # so you should at least not re-create or re-bind them.
+  attr_accessor :restored_session
+  # Contains the source ses on the first request after this
+  # session was cloned from another session.
+  attr_accessor :cloned_source 
+  # Contains the target sessions packed in an array on
+  # the first request after another session was cloned
+  # from this session.
+  attr_accessor :cloned_targets
+  # The session is not valid by default, it's set
+  # by SessionManager, if everything seems ok.
+  attr_accessor :ses_valid 
+  # The http request object.
+  attr_accessor :request 
+  # The http response object.
+  attr_accessor :response
+  # Response output.
+  attr_accessor :buffer
+  # The request success flag.
+  attr_accessor :response_success
+  # The 'special' browser's presense.
   attr_reader   :ie6
   
   
-  ### Message is initialized with a valid
-  ### Request and Response instance.
+  # Message is initialized with a valid +Request+ and +Response+ objects. 
+  # These are generated automatically by a webserver
   def initialize( request, response )
-    
-    # The http request object
     @request  = request
-    
-    # The request success flag
     @response_success = false
-    
-    # The http response object
     @response = response
-    
-    # Session data placeholder, assigned by SessionManager
     @session     = {}
-    
-    # response output
     @buffer = []
-    
-    # value response output
+    # Value response output.
     @value_buffer = []
-    
-    # The session identifier placeholder, assigned by SessionManager
     @ses_id      = 0
-    
-    # The session key placeholder
+    # The session key placeholder.
     @ses_key = false
-    
-    # The 'special' browser's presense is detected here:
     @ie6         = (request.header.has_key?('user-agent') and request.header['user-agent'].include?('MSIE 6.0'))
-    
-    # New session flag, check it in your code to decide
-    # what to do, when a new session is encountered.
-    # In plugins, this usually means that some Values
-    # need to be created and bound or possibly that a
-    # user_id mapping needs to be done.
     @new_session      = false
-    
-    # Old session first xhr flag, check it in your code
-    # to decide what to do, when a restored session is
-    # encountered. The old Values are automatically present,
-    # so you should at least not re-create or re-bind them.
     @restored_session = false
-    
-    # Contains the source ses on the first request after this
-    # session was cloned from another session.
     @cloned_source = false
-    
-    # Contains the target sessions packed in an array on
-    # the first request after another session was cloned
-    # from this session.
     @cloned_targets = false
-    
-    # The session is not valid by default, it's set
-    # by SessionManager, if everything seems ok.
     @ses_valid = false
-    
     @error_js = ''
-    
     # It's better to evaluate plain text than to respond with js.
     @response.content_type = 'text/javascript; charset=utf-8'
     @response['cache-control'] = 'no-cache'
@@ -109,15 +93,17 @@ class Message
     @response_sent = false
   end
   
-  ### Expire the session
+  # Expire the session.
   def expire_session
     $SESSION.expire_session( @ses_id )
   end
   
+  # Define the session key.
   def ses_key=(ses_key)
     @ses_key = ses_key
   end
   
+  # 
   def error_msg( error_js )
     @error_js = error_js
     # response_done
@@ -127,7 +113,7 @@ class Message
     buffer.to_json
   end
   
-  ## called to flush buffer
+  # called to flush buffer
   def response_done
     return if @response_sent
     if not @response_success
@@ -148,7 +134,7 @@ class Message
       
     end
     
-    ## flush the output
+    # flush the output
     if @do_gzip
       outp = GZString.new('')
       gzwriter = Zlib::GzipWriter.new(outp,Zlib::BEST_SPEED)
@@ -164,54 +150,58 @@ class Message
     @response_sent = true
   end
   
-  ### Sends some data to the client, usually
-  ### javascript, but is valid for any data.
+  # Sends data to the client, usually
+  # javascript, but is valid for any data.
   def reply(data)
     puts data if $config[:trace]
     @buffer.push( data )
   end
   
-  ### For valuemanager; insert changed values BEFORE other js.
+  # For valuemanager; insert changed values BEFORE other js.
   def reply_value(data)
     puts data if $config[:trace]
     @value_buffer.push( data )
   end
   
-  ### Sends data to the client's console
+  # Sends data to the client's console.
   def console(data)
     reply( "console.log(#{data.to_json});" )
   end
   
-  ### Sends a Magick::Image object to be served, returns a disposable uri.
+  # Serves an image object +img_obj+ by returning its disposable URL. The 
+  # second optional parameter +img_format+ defaults to 'PNG' and defines 
+  # the format of served picture.
   def serve_img( img_obj, img_format='PNG' )
     return $TICKETSERVE.serve_img( self, img_obj, img_format )
   end
   
-  ### Sends any binary to be served, returns a disposable uri.
+  # Sends any binary to be served, returns a disposable uri. First parameter
+  # defines the file data, second optional defines content type and defaults
+  # to 'text/plain' and third, also optional defines the filename which 
+  # defaults to 'untitled.txt'.
   def serve_file( file_data, content_type='text/plain', filename='untitled.txt' )
     return $TICKETSERVE.serve_file( self, file_data, content_type, filename )
   end
   
-  ### Sends any binary to be served, returns a static uri.
-  ###
-  ### IMPORTANT: PLEASE call unserve_rsrc manually, when you
-  ### don't need the resource anymore! Otherwise TicketServe will
-  ### keep on chugging more memory every time you serve something.
-  ###
-  ### HINT: Usually, it's a better idea to use serve_img or
-  ### serve_file instead.
-  ###
+  # Sends any binary to be served, returns a static uri.
+  #
+  # IMPORTANT: PLEASE call unserve_rsrc manually, when you
+  # don't need the resource anymore! Otherwise TicketServe will
+  # keep on chugging more memory every time you serve something.
+  #
+  # HINT: Usually, it's a better idea to use serve_img or
+  # serve_file instead.
   def serve_rsrc( rsrc_data, content_type='text/plain' )
     return $TICKETSERVE.serve_rsrc( self, rsrc_data, content_type )
   end
   
-  ### Removes the uri served, you HAVE TO call this manually when
-  ### you are done serving something!
+  # Removes the uri served, you HAVE TO call this manually when
+  # you are done serving something! Takes the uri as its only parameter.
   def unserve_rsrc( uri )
     $TICKETSERVE.del_rsrc( uri[3..-1] )
   end
   
-  ### Calls registered plugin +plugin+ method +plugin_method+ with any +args+
+  # Calls registered plugin +plugin+ method +plugin_method+ with any +args+
   def run( plugin_name, plug_method, *args )
     $PLUGINS.run_plugin( plugin_name, plug_method, *args)
   end
