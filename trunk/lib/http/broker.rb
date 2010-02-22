67c6df04ab684b31356af89ceede7723c8f6e3e5
Juha-Jarmo Heinonen <o@rsence.org>
2010-02-22 23:08:50 +0000

    Removed traces of ticket services from Broker (ticket services are available as the ticketservices plugin).
    git-svn-id: http://svn.rsence.org@925 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/lib/http/broker.rb
+++ trunk/lib/http/broker.rb
@@ -25,6 +25,15 @@ require 'http/restful'
 class Broker
   include RestfulDispatcher
   
+  def not_found
+    puts "/404: #{uri.inspect}" if $DEBUG_MODE
+    @response.status = 404
+    err404 = '<html><head><title>404 - Page Not Found</title></head><body>404 - Page Not Found</body></html>'
+    @response['content-type'] = 'text/html; charset=UTF-8'
+    @response['content-length'] = err404.length.to_s
+    @response.body = err404
+  end
+  
   ## Post requests are always xhr requests
   def post
     
@@ -32,9 +41,6 @@ class Broker
     
     ## The full request URI:
     uri = @request.fullpath
-    
-    ## User agent:
-    ua = @request.header['user-agent'] if $DEBUG_MODE
     
     uri_matched = $TRANSPORTER.servlet( :post, @request, @response )
     
@@ -46,33 +52,20 @@ class Broker
       if uri == broker_urls[:x]
         puts "/x: #{uri.inspect}" if $DEBUG_MODE
         $TRANSPORTER.xhr( @request, @response, false )
-  
+      
       ## /hello handles the first xhr (with cookies, for session key)
       elsif uri == broker_urls[:hello]
         puts "/hello: #{uri.inspect}" if $DEBUG_MODE
         $TRANSPORTER.xhr( @request, @response, true )
-  
+      
       ## /SOAP handles SOAP Requests
       elsif uri == broker_urls[:soap]
         puts "/SOAP: #{uri.inspect}"
         $TRANSPORTER.soap( @request, @response )
-  
-      # ## /up handles the uploads inited for ticketserve
-      # ## the second part of the uri contains the disposable
-      # ## upload key that has a server-side mapping to the
-      # ## user's session
-      # elsif uri[0..2] == broker_urls[:u]
-      #   puts "/U: #{uri.inspect}" if $DEBUG_MODE
-      #   $TICKETSERVE.upload( @request, @response )
       
       ## nothing matched, display 404
       else
-        puts "/404: #{uri.inspect}" if $DEBUG_MODE
-        @response.status = 404
-        err404 = '<html><head><title>404 - Page Not Found</title></head><body>404 - Page Not Found</body></html>'
-        @response['content-type'] = 'text/html; charset=UTF-8'
-        @response['content-length'] = err404.size.to_s
-        @response.body = err404
+        not_found
       end
     end
     
@@ -86,9 +79,6 @@ class Broker
     ## The full request URI:
     uri = @request.fullpath
     
-    ## User agent:
-    ua = @request.header['user-agent'] if $DEBUG_MODE
-    
     uri_matched = $TRANSPORTER.servlet( :get, @request, @response )
     
     unless uri_matched
@@ -101,47 +91,9 @@ class Broker
         puts "/H: #{uri.inspect}" if $DEBUG_MODE
         $FILESERVE.get( @request, @response )
       
-      # ## /i returns disposable RMagick objects rendered to data
-      # elsif uri.match( /^#{broker_urls[:i]}/ )
-      #   puts "/i: #{uri.inspect}" if $DEBUG_MODE
-      #   $TICKETSERVE.get( @request, @response, :img )
-      #     
-      # ## /d returns static data resources
-      # elsif uri.match( /^#{broker_urls[:d]}/ )
-      #   puts "/d: #{uri.inspect}" if $DEBUG_MODE
-      #   $TICKETSERVE.get( @request, @response, :rsrc )
-      #     
-      # ## /f return disposable data resources
-      # elsif uri.match( /^#{broker_urls[:f]}/ )
-      #   puts "/f: #{uri.inspect}" if $DEBUG_MODE
-      #   $TICKETSERVE.get( @request, @response, :file )
-      #     
-      # ## /B return smart data objects
-      # elsif uri.match( /^#{broker_urls[:b]}/ )
-      #   puts "/b: #{uri.inspect}" if $DEBUG_MODE
-      #   $TICKETSERVE.get( @request, @response, :blobobj )
-      #     
-      # ## special case for favicon
-      # elsif uri == broker_urls[:favicon]
-      #   $TICKETSERVE.favicon( @request, @response )
-      #     
-      # ## empty html page for the uploader iframe
-      # elsif uri == broker_urls[:uploader_iframe]
-      #   puts "/U/iframe_html: #{uri.inspect}" if $DEBUG_MODE
-      #   @response.status = 200
-      #   http_body = '<html><head><title>Empty Iframe for Uploading</title></head><body></body></html>'
-      #   @response['content-type'] = 'text/html; charset=UTF-8'
-      #   @response['content-length'] = http_body.size.to_s
-      #   @response.body = http_body
-      
       ## nothing matched, display 404
       else
-        puts "/404: #{uri.inspect}" if $DEBUG_MODE
-        @response.status = 404
-        err404 = '<html><head><title>404 - Page Not Found</title></head><body>404 - Page Not Found</body></html>'
-        @response['content-type'] = 'text/html; charset=UTF-8'
-        @response['content-length'] = err404.size.to_s
-        @response.body = err404
+        not_found
       end
     end
     
