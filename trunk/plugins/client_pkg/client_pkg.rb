6920422ea5b5f0fe6ce7c767f17dfeea760bddd5
Juha-Jarmo Heinonen <o@rsence.org>
2010-02-23 00:19:16 +0000

    added the missing headers
    git-svn-id: http://svn.rsence.org@930 5bb182ed-6ace-4cc9-85f1-9567b96542ab

--- trunk/plugins/client_pkg/client_pkg.rb
+++ trunk/plugins/client_pkg/client_pkg.rb
@@ -1,4 +1,10 @@
-
+##   Riassence Framework
+ #   Copyright 2007 Riassence Inc.
+ #   http://riassence.com/
+ #
+ #   You should have received a copy of the GNU General Public License along
+ #   with this software package. If not, contact licensing@riassence.com
+ ##
 
 class ClientPkg < Servlet
   
