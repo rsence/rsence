5db993811f22d71cc4d88bffd38cfc8adbbf688e
Toni Nurminen <toni.nurminen@rsence.org>
2010-03-14 20:28:44 +0200

    Fixed client in the Game of Life

--- demos/game_of_life/js/lifegrid.js
+++ demos/game_of_life/js/lifegrid.js
@@ -1,41 +1,37 @@
 LifeGrid = HControl.extend({
   lifeBlock: [],
   drawSubviews: function() {
+    var rowLength = 40;
+    var x_size = this.options.tile_height;
+    var y_size = this.options.tile_width;
     for (var i = 0; i < this.value.length; i++) {
-      var rowLength = 40;
-      var x_size = this.options.tile_height;
-      var y_size = this.options.tile_width;
+
       
       this.lifeBlock.push(ELEM.make( this.elemId, 'div' ));
       var top = (Math.floor(i / rowLength)) * (y_size + 2) + "px";
       var left = (i - (Math.floor(i / rowLength) * rowLength)) * (x_size + 2) + "px";
       console.log( left + " " + top);
-      if (this.value[i] > 0) {
+      if(this.value[i] > 0) {
         ELEM.setCSS( this.lifeBlock[i], "position:absolute;top:" + top + ";left:" + left + ";width:" + x_size + "px;height:" + y_size +"px;background-color:green;");
-        ELEM.setAttr( this.lifeBlock[i], "living", 1);
       } else {
         ELEM.setCSS( this.lifeBlock[i], "position:absolute;top:" + top + ";left:" + left + ";width:" + x_size + "px;height:" + y_size +"px;background-color:black;");
-        ELEM.setAttr( this.lifeBlock[i], "living", 0);
       }
     }
   },
   refreshValue: function() {
+    var rowLength = 40;
+    var x_size = this.options.tile_height;
+    var y_size = this.options.tile_width;
     for (var i = 0; i < this.value.length; i++) {
-      var rowLength = 40;
-      var x_size = this.options.tile_height;
-      var y_size = this.options.tile_width;
+      
       
       var top = (Math.floor(i / rowLength)) * (y_size + 2) + "px";
       var left = (i - (Math.floor(i / rowLength) * rowLength)) * (x_size + 2) + "px";
       console.log( left + " " + top);
-      if (this.value[i] > 0) {
-        if (ELEM.getAttr(this.lifeBlock[i], "living") == 0) {
-          ELEM.setCSS( this.lifeBlock[i], "position:absolute;top:" + top + ";left:" + left + ";width:" + x_size + "px;height:" + y_size +"px;background-color:green;");
-        }
+      if(this.value[i] > 0) {
+        ELEM.setCSS( this.lifeBlock[i], "position:absolute;top:" + top + ";left:" + left + ";width:" + x_size + "px;height:" + y_size +"px;background-color:green;");
       } else {
-        if (ELEM.getAttr(this.lifeBlock[i], "living") == 0) {
-          ELEM.setCSS( this.lifeBlock[i], "position:absolute;top:" + top + ";left:" + left + ";width:" + x_size + "px;height:" + y_size +"px;background-color:black;");
-        }
+        ELEM.setCSS( this.lifeBlock[i], "position:absolute;top:" + top + ";left:" + left + ";width:" + x_size + "px;height:" + y_size +"px;background-color:black;");
       }
     }
   }
