28a4a4a3767e0675a620100a4acfa65196d13cb7
Juha-Jarmo Heinonen <o@rsence.org>
2010-03-23 06:53:07 +0200

    Added support for client_pkgs.yaml as standard.

--- lib/plugins/gui_plugin.rb
+++ lib/plugins/gui_plugin.rb
@@ -32,6 +32,7 @@ class GUIPlugin < Plugin
   def init
     super
     @gui = GUIParser.new( self, @name )
+    @client_pkgs = false
   end
   
   # Extend this method to return custom params to GUIParser#init.
@@ -42,6 +43,78 @@ class GUIPlugin < Plugin
     return {
       :values => @gui.values( get_ses( msg ) )
     }
+  end
+  
+  def install_client_pkgs
+    if @client_pkgs
+      warn "install_client_pkgs: called with @client_pkgs defined"
+    end
+    client_pkgs_yaml = file_read( 'client_pkgs.yaml' )
+    if not client_pkgs_yaml
+      @client_pkgs = false
+    else
+      @client_pkgs = YAML.load( client_pkgs_yaml )
+      if @client_pkgs.has_key?(:src_dirs)
+        @client_pkgs[:src_dirs].each do |src_dir|
+          if src_dir.start_with?('./')
+            src_dir = File.join( @path, src_dir[2..-1] )
+          end
+          @plugins.client_pkg.add_src_dir( src_dir )
+        end
+      end
+      if @client_pkgs.has_key?(:packages)
+        @plugins.client_pkg.add_packages( @client_pkgs[:packages] )
+      end
+      if @client_pkgs.has_key?(:theme_names)
+        @plugins.client_pkg.add_themes( @client_pkgs[:theme_names] )
+      end
+      if @client_pkgs.has_key?(:gfx_formats)
+        @plugins.client_pkg.add_gfx_formats( @client_pkgs[:gfx_formats] )
+      end
+      if @client_pkgs.has_key?(:reserved_names)
+        @plugins.client_pkg.add_reserved_names( @client_pkgs[:reserved_names] )
+      end
+      @plugins.client_pkg.rebuild_client
+    end
+  end
+  
+  def uninstall_client_pkgs
+    if not @client_pkgs
+      warn "uninstall_client_pkgs: called without @client_pkgs defined"
+    else
+      if @client_pkgs.has_key?(:src_dirs)
+        @client_pkgs[:src_dirs].each do |src_dir|
+          if src_dir.start_with?('./')
+            src_dir = File.join( @path, src_dir[2..-1] )
+          end
+          @plugins.client_pkg.del_src_dir( src_dir )
+        end
+      end
+      if @client_pkgs.has_key?(:reserved_names)
+        @plugins.client_pkg.del_reserved_names( @client_pkgs[:reserved_names] )
+      end
+      if @client_pkgs.has_key?(:gfx_formats)
+        @plugins.client_pkg.del_gfx_formats( @client_pkgs[:gfx_formats] )
+      end
+      if @client_pkgs.has_key?(:theme_names)
+        @plugins.client_pkg.del_themes( @client_pkgs[:theme_names] )
+      end
+      if @client_pkgs.has_key?(:packages)
+        @plugins.client_pkg.del_packages( @client_pkgs[:packages].keys )
+      end
+      @plugins.client_pkg.rebuild_client
+    end
+    @client_pkgs = false
+  end
+  
+  def open
+    super
+    install_client_pkgs if File.exist?( File.join( @path, 'client_pkgs.yaml' ) )
+  end
+  
+  def close
+    super
+    uninstall_client_pkgs if @client_pkgs
   end
   
   # Sends gui specification to the main plugin
