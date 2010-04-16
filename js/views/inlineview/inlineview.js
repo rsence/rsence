33dae08e4bb8e50709c078618e54b15c7829ad2d
Juha-Jarmo Heinonen <o@rsence.org>
2010-04-16 05:00:11 +0300

    Added HInlineView for displaying inline content.

---
+++ js/views/inlineview/inlineview.js
@@ -0,0 +1,14 @@
+/*   Riassence Framework
+ *   Copyright 2010 Riassence Inc.
+ *   http://riassence.com/
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this software package. If not, contact licensing@riassence.com
+ */
+
+// Simple view for inline content (relative positioning and inline view instead of block)
+HInlineView = HView.extend({
+  isAbsolute: false,
+  isRelative: true,
+  displayMode: 'inline'
+});
