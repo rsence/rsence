ff5680c9edb217d8d59898e153bd0f0c301048ed
Juha-Jarmo Heinonen <o@rsence.org>
2010-05-12 20:24:19 +0300

    wrote stub for the Dependencies class

--- lib/plugins/dependencies.rb
+++ lib/plugins/dependencies.rb
@@ -5,3 +5,32 @@
  #   You should have received a copy of the GNU General Public License along
  #   with this software package. If not, contact licensing@riassence.com
  ##
+
+module RSence
+  class Dependencies
+    def initialize( deps = [] )
+      @deps = deps
+      @depends_on = {
+        # :name => [ :dep1, :dep2, :dep3, ... ]
+      }
+      # ...additional initialization?
+    end
+    def set_deps( name, deps )
+      if not @depends_on.has_key?(name)
+        @depends_on[name] = deps
+      else
+        @depends_on[name].join( deps )
+      end
+      # ...additional checks?
+    end
+    alias set_dependencies set_deps
+    def deps
+      # calculate @depends_on and push items with
+      # resolved dependencies into @deps
+      # ...
+      return @deps
+    end
+    alias deps load_order
+  end
+end
+
