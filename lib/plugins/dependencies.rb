bdc113dac0e25a75220e11155a682e1dd7d4ec56
Toni Nurminen <toni.nurminen@rsence.org>
2010-05-12 19:58:00 +0300

    Dependency tracker

---
+++ lib/plugins/dependencies.rb
@@ -0,0 +1,7 @@
+##   Riassence Framework
+ #   Copyright 2010 Riassence Inc.
+ #   http://riassence.com/
+ #
+ #   You should have received a copy of the GNU General Public License along
+ #   with this software package. If not, contact licensing@riassence.com
+ ##
