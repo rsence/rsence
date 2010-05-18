c7ec6dc7e7d225ddafd453e8df7847a6a3284c69
Juha-Jarmo Heinonen <o@rsence.org>
2010-05-18 05:47:55 +0300

    cleanup

--- plugins/index_html/index_html.rb
+++ plugins/index_html/index_html.rb
@@ -6,11 +6,7 @@
  #   with this software package. If not, contact licensing@riassence.com
  ##
 
-=begin
- IndexHtmlPlugin is the servlet plugin that is responsible for initializing the "boot-strap page".
- 
- It just loads, caches and sends the page for now.
-=end
+# IndexHtmlPlugin is the servlet plugin that is responsible for initializing the "boot-strap page".
 class IndexHtmlPlugin < Servlet
   
   def match( uri, method )
@@ -31,17 +27,11 @@ class IndexHtmlPlugin < Servlet
   end
   
   def open
-    #@deps = []
     @index_html_src = file_read( ::RSence.config[:index_html][:index_tmpl] )
-    # loading_gif = File.read( File.join( @path, 'img/loading.gif' ) )
-    # @loading_gif_id = @plugins[:ticketservices].serve_rsrc( loading_gif, 'image/gif' )
-    # riassence_gif = File.read( File.join( @path, 'img/riassence.gif' ) )
-    # @riassence_gif_id = $TICKETSERVE.serve_rsrc( riassence_gif, 'image/gif' )
     render_index_html
   end
   
   def close
-    # $TICKETSERVE.del_rsrc( @riassence_gif_id )
     @plugins[:ticket].del_rsrc( @loading_gif_id )
   end
   
@@ -50,8 +40,6 @@ class IndexHtmlPlugin < Servlet
     index_html = @index_html_src.clone
     
     index_html.gsub!('__DEFAULT_TITLE__',::RSence.config[:index_html][:title])
-    # @index_html.gsub!('__LOADING_GIF_ID__',@loading_gif_id)
-    # @index_html.gsub!('__RIASSENCE_GIF_ID__',@riassence_gif_id)
     client_rev = @plugins[:client_pkg].client_cache.client_rev
     index_html.gsub!('__CLIENT_REV__',client_rev)
     index_html.gsub!('__CLIENT_BASE__',File.join(::RSence.config[:broker_urls][:h],client_rev))
@@ -63,16 +51,6 @@ class IndexHtmlPlugin < Servlet
       deps_src += %{<script src="#{dep}" type="text/javascript"></script>}
     end
     index_html.gsub!('__SCRIPT_DEPS__',deps_src)
-    
-    # @index_gzip = GZString.new('')
-    # gzwriter = Zlib::GzipWriter.new( @index_gzip, Zlib::BEST_SPEED )
-    # gzwriter.write( @index_html )
-    # gzwriter.close
-    
-    # @content_size = @index_html.size
-    # @content_size_gzip = @index_gzip.size
-    
-    # @index_date = httime( Time.now )
     
     return index_html
   end
@@ -97,29 +75,20 @@ class IndexHtmlPlugin < Servlet
       )
       buffer += msg.value_buffer
       msg.buffer.each do |buffer_item|
-        # if ::RSence.config[:]
-        # buffer.push( "qP(function(){#{buffer_item};console.log(#{buffer_item.to_json});});")
         buffer.push( "qP(function(){#{buffer_item};});")
       end
       ses_key = msg.ses_key
-      # buffer = [ msg.ses_key, msg.value_buffer, msg.buffer ]
     end
     buffer.unshift( "COMM.Session.newKey(#{ses_key.to_json});" )
     buffer.unshift( "COMM.Session.sha_key=#{sha_key.to_json};" )
     buffer.unshift( "COMM.Session.req_num=#{req_num};" )
-    # buffer.each {|b|puts b}
-    # require 'pp'; pp buffer
     index_html = render_index_html
     return index_html.gsub('__STARTUP_SEQUENCE__', buffer.join("\n") )
   end
   
   ## Outputs a static web page.
   def get( request, response, ses )
-    # puts "index_html"
-    # index_html = session_index_html( request, response )
     index_html = render_index_html
-    # index_html = index_html.encode(Encoding::BINARY)
-    
     support_gzip = (request.header.has_key?('accept-encoding') and \
                     request.header['accept-encoding'].include?('gzip')) \
                     and not ::RSence.config[:no_gzip]
