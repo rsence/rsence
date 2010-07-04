090cafbca9beaf7b97d37987403655200a17d905
Juha-Jarmo Heinonen <o@rsence.org>
2010-07-04 12:29:55 +0300

    minor code cleanup

--- lib/util/gzstring.rb
+++ lib/util/gzstring.rb
@@ -1,7 +1,9 @@
+
 require 'zlib'
 
-# Implements the +write+ method for strings, used with zlib to
-# use GZStrings as the target for compression.
+
+# @private Implements the +write+ method for strings, used with zlib to
+#   use GZStrings as the target for compression.
 class GZString < String
   alias write <<
 end
