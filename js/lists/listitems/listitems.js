68fc559bf78ca1bc109add73f13bbb9cc9ef2c9a
Juha-Jarmo Heinonen <o@rsence.org>
2010-07-04 12:08:09 +0300

    Added support for flat arrays with numbers or strings

--- js/lists/listitems/listitems.js
+++ js/lists/listitems/listitems.js
@@ -9,10 +9,19 @@
 
 /*** = Description
   ** HListItems is uses an array-packed list of hash objects as its value.
+  ##
+  ## == Value as Array of Hashes:
   ** Each item in the array should have a 'label' and a 'value' key.
   ** The 'label' key of each item is used as the label for the HRadiobutton in the list.
   ** The 'value' key of each item is the value used for the output.
+  ##
+  ## == Value as Array of strings or numerals
+  ## Each Item like with hash, except the item is both label and value. Each item should
+  ## be unique.
+  ##
+  ## == Important
   ** The parent object of a HListItem needs to be a compatible hash, like HRadiobuttonList.
+  ##
   ***/
 var//RSence.Lists
 HListItems = HValueResponder.extend({
@@ -41,12 +50,15 @@ HListItems = HValueResponder.extend({
   refresh: function(){
     if ( this.value instanceof Array ) {
       var _listItems = [],
-          _row,
+          _row, _rowType,
           _label, _value,
           i = 0;
       for ( ; i < this.value.length ; i++ ){
         _row = this.value[i];
-        if ( _row instanceof Object ) {
+        _rowType = COMM.Values.type( _row );
+        // console.log('row:',_row,' rowType:',_rowType);
+        // hashes
+        if ( _rowType === 'h' ) {
           _label = _row['label'];
           _value = _row['value'];
           if ( _label === undefined || _value === undefined ){
@@ -54,8 +66,14 @@ HListItems = HValueResponder.extend({
           }
           _listItems.push( [_value, _label] );
         }
+        // strings and integers
+        else if ( ['s','n'].indexOf(_rowType) !== -1 ){
+          _label = _row.toString();
+          _value = _row;
+          _listItems.push( [_value, _label] );
+        }
         else {
-          this._warningMessage( "The row "+_row+" is not an object (ignored)" );
+          this._warningMessage( "The row "+_row+" is has unsupported row type: '"+_rowType+"' (ignored)" );
         }
       }
       this.parent.setListItems( _listItems );
