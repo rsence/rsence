c183fe08ff8a8e2872ff2a61d4b009d972e582b1
Juha-Jarmo Heinonen <o@rsence.org>
2012-09-04 07:06:20 +0300

    Added timeout to addr+port test

--- lib/rsence/argv/test_port.rb
+++ lib/rsence/argv/test_port.rb
@@ -6,20 +6,26 @@ module ArgvUtil
   # Automatically replaces '0.0.0.0' with '127.0.0.1'
   def test_port( port, addr='127.0.0.1' )
     require 'socket'
+    require 'timeout'
     begin
       addr = '127.0.0.1' if addr == '0.0.0.0'
-      if RUBY_VERSION.to_f >= 1.9
-        sock = TCPSocket.open( addr, port )
-      else
-        begin
-          sock = TCPsocket.open( addr, port )
-        rescue NameError => e
-          warn "TCPsocket not available, trying TCPSocket.."
+      timeout(1) do
+        if RUBY_VERSION.to_f >= 1.9
           sock = TCPSocket.open( addr, port )
+        else
+          begin
+            sock = TCPsocket.open( addr, port )
+          rescue NameError => e # Rubinius
+            warn "TCPsocket not available, trying TCPSocket.."
+            sock = TCPSocket.open( addr, port )
+          end
         end
+        sock.close
       end
-      sock.close
       return true
+    rescue Timeout::Error
+      puts "Address #{addr} port #{port} timed out"
+      return false
     rescue Errno::ECONNREFUSED
       return false
     rescue => e
@@ -30,3 +36,4 @@ module ArgvUtil
 
 end
 end
+
