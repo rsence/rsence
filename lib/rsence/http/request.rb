3e2764e96e133560cecd6d11ad1a989410d5f265
Juha-Jarmo Heinonen <o@rsence.org>
2013-06-27 09:45:13 +0300

    Proper ENV to request headers handling

--- lib/rsence/http/request.rb
+++ lib/rsence/http/request.rb
@@ -1,6 +1,6 @@
 
 module RSence
-  
+
   # Simple Request class, slightly more involved
   # than the Rack::Request it's extending.
   class Request < Rack::Request
@@ -20,38 +20,26 @@ module RSence
     def unparsed_uri
       return @header['request-uri']
     end
+    @@env_transl = {
+      'SERVER_NAME'     => 'server-name',
+      'PATH_INFO'       => 'path-info',
+      'SERVER_PROTOCOL' => 'server-protocol',
+      'REQUEST_PATH'    => 'request-path',
+      'SERVER_SOFTWARE' => 'server-software',
+      'REMOTE_ADDR'     => 'remote-addr',
+      'REQUEST_URI'     => 'request-uri',
+      'SERVER_PORT'     => 'server-port',
+      'QUERY_STRING'    => 'query-string',
+      'REQUEST_METHOD'  => 'request-method'
+    }
     def env2header
-      [ ['SERVER_NAME',           'server-name'],
-        ['HTTP_USER_AGENT',       'user-agent'],
-        ['HTTP_ACCEPT_ENCODING',  'accept-encoding'],
-        ['PATH_INFO',             'path-info'],
-        ['HTTP_HOST',             'host'],
-        ['HTTP_ACCEPT_LANGUAGE',  'accept-language'],
-        ['SERVER_PROTOCOL',       'server-protocol'],
-        ['REQUEST_PATH',          'request-path'],
-        ['HTTP_KEEP_ALIVE',       'keep-alive'],
-        ['SERVER_SOFTWARE',       'server-software'],
-        ['REMOTE_ADDR',           'remote-addr'],
-        ['HTTP_REFERER',          'referer'],
-        ['HTTP_VERSION',          'version'],
-        ['HTTP_ACCEPT_CHARSET',   'accept-charset'],
-        ['REQUEST_URI',           'request-uri'],
-        ['SERVER_PORT',           'server-port'],
-        ['QUERY_STRING',          'query-string'],
-        ['HTTP_ACCEPT',           'accept'],
-        ['REQUEST_METHOD',        'request-method'],
-        ['HTTP_CONNECTION',       'connection'],
-        ['HTTP_SOAPACTION',       'soapaction'],
-        ['HTTP_FORWARDED_HOST',   'forwarded-host']
-      ].each do |env_key,header_key|
-        if @env.has_key?(env_key)
-          @header[header_key] = @env[env_key]
-        end
-        if env_key.start_with?( 'HTTP_' )
-          x_env_key = "HTTP_X#{env_key[4..-1]}"
-          if @env.has_key?( x_env_key )
-            @header["x-#{header_key}"] = @env[ x_env_key ]
-          end
+      @env.each do |env_key,env_val|
+        if @@env_transl.has_key?(env_key)
+          http_key = @@env_transl[env_key]
+          @header[http_key] = @env[env_key]
+        elsif env_key.start_with?( 'HTTP_' )
+          http_key = env_key[4..-1].gsub(/_([A-Z0-9]+)/) {|m|'-'+$1.downcase}[1..-1]
+          @header[http_key] = @env[env_key]
         end
       end
     end
